c2ae6d64e24003089cf7116071ed4924f78faa36
majidsajadi <majid.sajadi@tapsi.cab>
2024-02-18 13:31:44 +0330

    add storybook

---
+++ src/modal/modal.stories.ts
@@ -0,0 +1,31 @@
+import { html, TemplateResult } from "lit";
+import "./index.js";
+
+export default {
+  title: "Modal",
+  component: "tap-modal",
+  argTypes: {
+    open: { control: "boolean" },
+  },
+};
+
+interface Story<T> {
+  (args: T): TemplateResult;
+  args?: Partial<T>;
+  argTypes?: Record<string, unknown>;
+}
+
+interface ArgTypes {
+  open: boolean;
+  slot?: TemplateResult;
+}
+
+const Template: Story<ArgTypes> = ({ open, slot }: ArgTypes) => html`
+  <tap-modal .open=${open}> ${slot} </tap-modal>
+`;
+
+export const Modal = Template.bind({});
+
+Modal.args = {
+  open: true,
+};
