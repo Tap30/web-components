590df32ce71973e74dc392f6697de4ab5bc2d7e0
majidsajadi <pakzadianmrp@gmail.com>
2024-03-09 14:28:55 +0330

    add text field styles

---
+++ src/text-field/index.ts
@@ -0,0 +1,14 @@
+import { customElement } from "lit/decorators.js";
+import { TextField } from "./text-field";
+import styles from "./text-field.style";
+
+@customElement("tap-text-field")
+export class TapTextField extends TextField {
+  static readonly styles = [styles];
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    "tap-text-field": TapTextField;
+  }
+}
