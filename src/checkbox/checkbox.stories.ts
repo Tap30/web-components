287222277098186bca7ae26d0b739a2f7996617a
majidsajadi <pakzadianmrp@gmail.com>
2024-03-10 18:39:39 +0330

    add checkbox component

---
+++ src/checkbox/checkbox.stories.ts
@@ -0,0 +1,24 @@
+import { html, TemplateResult } from "lit";
+import "./index.js";
+
+export default {
+  title: "Checkbox",
+  component: "tap-checkbox",
+  argTypes: {},
+};
+
+interface Story<T> {
+  (args: T): TemplateResult;
+  args?: Partial<T>;
+  argTypes?: Record<string, unknown>;
+}
+
+interface ArgTypes {}
+
+const Template: Story<ArgTypes> = ({}: ArgTypes) => html`
+  <tap-checkbox></tap-checkbox>
+`;
+
+export const Checkbox = Template.bind({});
+
+Checkbox.args = {};
