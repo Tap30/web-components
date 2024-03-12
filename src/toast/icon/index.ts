a1b9371a419876cdaced9301d5b4114c9b446f18
Amirhossein Alibakhshi <amir78729@gmail.com>
2024-03-12 18:22:04 +0330

    feat: impelement toast component

---
+++ src/toast/icon/index.ts
@@ -0,0 +1,14 @@
+import circleCheckFill from "./circle-check-fill.svg";
+import circleExclamationFill from "./circle-exclamation-fill.svg";
+import circleInformationFill from "./circle-information-fill.svg";
+import triangleExclamationFill from "./triangle-exclamation-fill.svg";
+import defaultIcon from "./default.svg";
+
+// TODO: fix in icon library
+export {
+  circleCheckFill,
+  circleExclamationFill,
+  circleInformationFill,
+  triangleExclamationFill,
+  defaultIcon,
+};
