83229c4f8d278424dcd5bc01022f00345db0df6c
majidsajadi <majidsajadi93@gmail.com>
2024-04-06 18:50:59 +0330

    feat: add step indicator component

---
+++ src/step-indicator/index.ts
@@ -0,0 +1,14 @@
+import { customElement } from "lit/decorators.js";
+import { StepIndicator } from "./step-indicator.js";
+import styles from "./step-indicator.style.js";
+
+@customElement("tap-step-indicator")
+export class TapSegmentedButtonGroup extends StepIndicator {
+  static readonly styles = [styles];
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    "tap-step-indicator": TapSegmentedButtonGroup;
+  }
+}
---
+++ src/step-indicator/step-indicator.stories.ts
@@ -0,0 +1,24 @@
+import { html, TemplateResult } from "lit";
+import "./index.js";
+
+export default {
+  title: "Step Indicator",
+  component: "tap-step-indicator",
+  argTypes: {},
+};
+
+interface Story<T> {
+  (args: T): TemplateResult;
+  args?: Partial<T>;
+  argTypes?: Record<string, unknown>;
+}
+
+interface ArgTypes {}
+
+const Template: Story<ArgTypes> = ({}: ArgTypes) => html`
+  <tap-step-indicator .steps=${5}></tap-step-indicator>
+`;
+
+export const StepIndicator = Template.bind({});
+
+StepIndicator.args = {};
