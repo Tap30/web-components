ee19510374084240d76e3213da0d6236d581b4a1
majidsajadi <majidsajadi93@gmail.com>
2024-04-07 17:00:00 +0330

    feat: add divider

---
+++ src/divider/divider.stories.ts
@@ -0,0 +1,26 @@
+import { html, TemplateResult } from "lit";
+import "./index.js";
+
+export default {
+  title: "Divider",
+  component: "tap-divider",
+  argTypes: {},
+};
+
+interface Story<T> {
+  (args: T): TemplateResult;
+  args?: Partial<T>;
+  argTypes?: Record<string, unknown>;
+}
+
+interface ArgTypes {}
+
+const Template: Story<ArgTypes> = ({}: ArgTypes) => html`
+  <tap-divider type="thin"> </tap-divider>
+  <tap-divider> </tap-divider>
+  <tap-divider type="bold"> </tap-divider>
+`;
+
+export const Divider = Template.bind({});
+
+Divider.args = {};
