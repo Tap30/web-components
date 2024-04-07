394795dae9b26d191b23e38084113bf36404e6cf
majidsajadi <majidsajadi93@gmail.com>
2024-04-07 17:00:00 +0330

    feat: add divider

---
+++ src/divider/divider.ts
@@ -0,0 +1,11 @@
+import { LitElement } from "lit";
+import { property } from "lit/decorators.js";
+
+export class Divider extends LitElement {
+  @property({ reflect: true }) type: "thin" | "medium" | "bold" = "medium";
+
+  connectedCallback() {
+    super.connectedCallback();
+    this.setAttribute("role", "separator");
+  }
+}
---
+++ src/divider/index.ts
@@ -0,0 +1,14 @@
+import { customElement } from "lit/decorators.js";
+import { Divider } from "./divider.js";
+import styles from "./divider.style.js";
+
+@customElement("tap-divider")
+export class TapDivider extends Divider {
+  static readonly styles = [styles];
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    "tap-divider": TapDivider;
+  }
+}
