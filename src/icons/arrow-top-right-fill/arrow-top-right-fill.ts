07cf8682fcb4112a6136dc3764317790e06f11c7
Amir Hossein Alibakhshi <amir78729@gmail.com>
2024-04-15 19:24:40 +0330

    fix: replace size with height and width

--- src/icons/arrow-top-right-fill/arrow-top-right-fill.ts
+++ src/icons/arrow-top-right-fill/arrow-top-right-fill.ts
@@ -1,7 +1,7 @@
 import {html} from "lit";
 import {TapIcon} from "../../icon";
 
-export class ArrowTopRight1Icon extends TapIcon {
+export class ArrowTopRightFillIcon extends TapIcon {
   render() {
     return this.renderIcon(html`
       <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
