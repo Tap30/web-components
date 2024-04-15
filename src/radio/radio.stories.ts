b2788df9fbfc8b5860fc505b636ffd25fa805000
Alireza <pakzadianmrp@gmail.com>
2024-04-15 15:53:52 +0330

    feat: add radio component

---
+++ src/radio/radio.stories.ts
@@ -0,0 +1,24 @@
+import { html, TemplateResult } from "lit";
+import "./index.js";
+
+export default {
+  title: "Radio",
+  component: "tap-radio",
+  argTypes: {},
+};
+
+interface Story<T> {
+  (args: T): TemplateResult;
+  args?: Partial<T>;
+  argTypes?: Record<string, unknown>;
+}
+
+interface ArgTypes {}
+
+const Template: Story<ArgTypes> = ({}: ArgTypes) => html`
+  <tap-radio></tap-radio>
+`;
+
+export const Radio = Template.bind({});
+
+Radio.args = {};
