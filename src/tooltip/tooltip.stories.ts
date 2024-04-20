4ca295ffb2250b21d90db47f7716d22f2c0580e3
zeynab.nasiri <zeinab.nasiri1996@gmail.com>
2024-04-20 20:27:20 +0330

    feat: add tooltip story

--- src/tooltip/tooltip.stories.ts
+++ src/tooltip/tooltip.stories.ts
@@ -1,1 +1,35 @@
-import { html, TemplateResult } from "lit";
+import {html, TemplateResult} from "lit";
+import "./index.js";
+import "../tooltip";
+
+export default {
+  title: "Tooltip",
+  component: "tap-tooltip",
+  argTypes: {
+    
+  },
+};
+
+interface Story<T> {
+  (args: T): TemplateResult;
+  args?: Partial<T>;
+  argTypes?: Record<string, unknown>;
+}
+
+interface ArgTypes {
+  
+}
+
+const Template: Story<ArgTypes> = ({
+
+}) => {
+  return html`
+    <tap-tooltip>
+    </tap-tooltip>
+  `;
+}
+
+export const Simple = Template.bind({});
+Simple.args = {
+
+};
