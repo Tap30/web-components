2f738954ab528374e89d3a056b862ea00b7025ba
zeynab.nasiri <zeinab.nasiri1996@gmail.com>
2024-04-20 20:26:54 +0330

    fix: fix custom element name

--- src/tooltip/index.ts
+++ src/tooltip/index.ts
@@ -2,7 +2,7 @@ import { customElement } from "lit/decorators.js";
 import { Tooltip } from "./tooltip";
 import styles from "./tooltip.style";
 
-@customElement("tooltip")
+@customElement("tap-tooltip")
 export class TapTooltip extends Tooltip {
   static readonly styles = [styles];
 }
