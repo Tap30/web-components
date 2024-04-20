3895351b86b6bdec6cb8030b13887892d4e8aec9
zeynab.nasiri <zeinab.nasiri1996@gmail.com>
2024-04-20 22:10:42 +0330

    feat: add button and dynamic label to tooltip

--- src/tooltip/tooltip.style.ts
+++ src/tooltip/tooltip.style.ts
@@ -1,5 +1,13 @@
 import { css } from "lit";
 
 export default css`
+  .tooltip {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+  }
 
+  .tooltip-label {
+
+  }
 `;
--- src/tooltip/tooltip.ts
+++ src/tooltip/tooltip.ts
@@ -1,5 +1,6 @@
 import { html, LitElement } from "lit";
 import { property } from "lit/decorators.js"
+import "../button/index";
 
 export class Tooltip extends LitElement {
   static readonly shadowOptions = {
@@ -12,9 +13,21 @@ export class Tooltip extends LitElement {
 
   @property({ type: String }) pointer?: 'top' | 'bottom' | 'left' | 'right' = 'bottom';
 
+  private renderDismissIcon() {
+    return html`
+      <svg width="25" height="24" viewBox="0 0 25 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path fill-rule="evenodd" clip-rule="evenodd" d="M18.1568 7.75735L13.9142 12L18.1568 16.2426L16.7426 17.6568L12.5 13.4142L8.25735 17.6568L6.84314 16.2426L11.0858 12L6.84314 7.75735L8.25735 6.34314L12.5 10.5858L16.7426 6.34314L18.1568 7.75735Z" fill="currentColor"/>
+      </svg>
+    `
+  }
+
   render() {
     return html`
-      <div>
+      <div class="tooltip">
+        <tap-button size="small" variant="naked" shape="circle" .icon=${this.renderDismissIcon()}></tap-button>
+        <div class="tooltip-label">
+          <slot></slot>
+        </div>
       </div>
     `;
   }
