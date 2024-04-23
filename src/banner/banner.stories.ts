9998e97ca678c7a6c9a57e94b0cb164ee1f3e7b4
majidsajadi <majidsajadi93@gmail.com>
2024-04-23 14:29:34 +0330

    feat: add banner components

---
+++ src/banner/banner.stories.ts
@@ -0,0 +1,55 @@
+import { html, TemplateResult } from "lit";
+import "./index.js";
+import "../button/index.js";
+
+export default {
+  title: "Banner",
+  component: "tap-banner",
+  argTypes: {},
+};
+
+interface Story<T> {
+  (args: T): TemplateResult;
+  args?: Partial<T>;
+  argTypes?: Record<string, unknown>;
+}
+
+interface ArgTypes {}
+
+const Template: Story<ArgTypes> = ({}: ArgTypes) => html`
+  <tap-banner
+    heading="با ۱۶۳۰، تلفنی تپسی بگیر"
+    description="درخواست خودرو بدون نیاز به اینترنت"
+    image="https://able-media.tapsi.cab/statics/N31LNC95J6MCPIYB0D.jpg"
+    background-color="rgb(255, 213, 194)"
+    text-color="rgb(66, 46, 40)"
+  >
+    <tap-button size="small">دریافت تخفیف</tap-button>
+  </tap-banner>
+
+  <tap-banner
+variant="hero"
+    heading="با ۱۶۳۰، تلفنی تپسی بگیر"
+    description="درخواست خودرو بدون نیاز به اینترنت"
+    image="https://able-media.tapsi.cab/statics/N31LNC95J6MCPIYB0D.jpg"
+    background-color="rgb(255, 213, 194)"
+    text-color="rgb(66, 46, 40)"
+  >
+    <tap-button size="small" slot="extra" >majid</tap-button>
+    <tap-button size="small">دریافت تخفیف</tap-button>
+  </tap-banner>
+<br />
+  <tap-banner
+variant="hero"
+    heading="با ۱۶۳۰، تلفنی تپسی بگیر"
+    description="درخواست خودرو بدون نیاز به اینترنت"
+    image="https://able-media.tapsi.cab/statics/N31LNC95J6MCPIYB0D.jpg"
+    background-color="rgb(255, 213, 194)"
+    text-color="rgb(66, 46, 40)"
+  >
+  </tap-banner>
+`;
+
+export const Banner = Template.bind({});
+
+Banner.args = {};
