6c901dbaa0f90d8480c426a4b4eefd8916a5c772
zeynab.nasiri <zeinab.nasiri1996@gmail.com>
2024-04-27 12:58:01 +0330

    fix: fix UI issues

--- src/tooltip/tooltip.style.ts
+++ src/tooltip/tooltip.style.ts
@@ -5,9 +5,11 @@ export default css`
     display: flex;
     flex-direction: row;
     align-items: center;
+    background-color: var(--tap-palette-gray-700);
+    border-radius: var(--radius-8px, 8px);
   }
 
   .tooltip-label {
-
+    color: vat(--tap-palette-white);
   }
 `;
