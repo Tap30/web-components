2aaeabe043e7c7226a70b05be6a05dec812ab784
Mohammad Seyfayi <madsams024@gmail.com>
2024-04-28 14:13:11 +0330

    style: add eslint plugin lit and wc

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,15 @@
 import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
 import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+import eslintLit from 'eslint-plugin-lit';
+import eslintWC from 'eslint-plugin-wc';
 
 export default tseslint.config(
   eslint.configs.recommended,
   ...tseslint.configs.recommendedTypeChecked,
-  ...eslintPluginPrettierRecommended,
+  eslintPluginPrettierRecommended,
+  { plugins: { lit: eslintLit }, rules: eslintLit.configs.recommended.rules },
+  eslintWC.configs['flat/recommended'],
   {
     languageOptions: {
       parserOptions: {
