98fe1793e3ea50847a56eea57e5425d42036be84
mohammadreza pakzadian <pakzadianmrp@gmail.com>
2024-04-30 16:24:39 +0330

    fix: resolve comments

--- src/pinwheel/pinwheel.style.ts
+++ src/pinwheel/pinwheel.style.ts
@@ -16,30 +16,30 @@ export default css`
     display: none;
   }
 
-  div > div {
+  .pinwheel > div {
     padding: var(--tap-sys-spacing-0) var(--tap-sys-spacing-6);
     /* FIXME: we dont have sizing tokens yet  */
     height: 48px;
     font: inherit;
-    font-family: var(--tap-sys-font-family);
-    line-height: var(--tap-sys-typography-body-md-height);
-    font-size: var(--tap-sys-typography-body-md-size);
-    font-weight: var(--tap-sys-typography-body-md-weight);
-    color: var(--tap-sys-color-content-tertiary);
+    font-family: var(--tap-font-family, var(--tap-sys-font-family)), serif;
+    line-height: var(--tap-pinwheel-typography-body-md-height, var(--tap-sys-typography-body-md-height));
+    font-size: var(--tap-pinwheel-typography-body-md-size, var(--tap-sys-typography-body-md-size));
+    font-weight: var(--tap-pinwheel-typography-body-md-weight, var(--tap-sys-typography-body-md-weight));
+    color: var(--tap-pinwheel-color-content-tertiary, var(--tap-sys-color-content-tertiary));
   }
 
   .pinwheel div:first-child {
-    padding-top: var(--tap-sys-spacing-11);
+    padding-top: var(--tap-pinwheel-spacing-11, var(--tap-sys-spacing-11));
   }
 
   .pinwheel div:last-child {
-    padding-bottom: var(--tap-sys-spacing-11);
+    padding-bottom: var(--tap-pinwheel-spacing-11, var(--tap-sys-spacing-11));
   }
 
-  .active {
-    line-height: var(--tap-sys-typography-label-md-height);
-    font-size: var(--tap-sys-typography-label-md-size);
-    font-weight: var(--tap-sys-typography-label-md-weight);
-    color: var(--tap-sys-color-content-primary);
+  .pinwheel .active {
+    line-height: var(--tap-pinwheel-typography-label-md-height, var(--tap-sys-typography-label-md-height));
+    font-size: var(--tap-pinwheel-typography-label-md-size, var(--tap-sys-typography-label-md-size));
+    font-weight: var(--tap-pinwheel-typography-label-md-weight, var(--tap-sys-typography-label-md-weight));
+    color: var(--tap-pinwheel-color-content-primary, var(--tap-sys-color-content-primary));
   }
 `;
