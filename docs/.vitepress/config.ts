f20a76e5c1cde76fbc240ea794ed3314ff2889f7
majidsajadi <majidsajadi93@gmail.com>
2024-05-08 16:39:39 +0330

    docs: read elements on build

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,13 +1,31 @@
 import fs from 'node:fs';
 import { defineConfig } from 'vitepress';
+import type { Package } from 'custom-elements-manifest';
 
-const components = fs.readdirSync('src').map((name) => ({
-  link: '/components/' + name,
-  text: name
-    .split('-')
-    .map((name) => name.charAt(0).toUpperCase() + name.slice(1))
-    .join(' '),
-}));
+const file = fs.readFileSync('custom-elements.json');
+const manifest = JSON.parse(file.toString()) as Package;
+
+const components = manifest.modules
+  .filter((module) => !module.path.startsWith('src/icon'))
+  .map((module) => {
+    if (!module.exports)
+      throw new Error(`Module has no export: ${module.path}`);
+
+    const components = module.exports.filter(
+      (exp) => exp.kind === 'custom-element-definition',
+    );
+    // For now we asume we have only one custom element per moduel
+    const component = components[0];
+
+    if (!component) {
+      throw new Error(`No comopnent found in the module: ${module.path}`);
+    }
+
+    return {
+      link: '/components/' + component.name,
+      text: component.declaration.name,
+    };
+  });
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
