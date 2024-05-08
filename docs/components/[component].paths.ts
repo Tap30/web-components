dd622c52fe871868945a0c6fa6d998251d591ffd
majidsajadi <majidsajadi93@gmail.com>
2024-05-08 16:39:39 +0330

    docs: read elements on build

--- docs/components/[component].paths.ts
+++ docs/components/[component].paths.ts
@@ -1,9 +1,37 @@
 import fs from 'node:fs';
+import type { Package, CustomElement } from 'custom-elements-manifest';
 
 export default {
   paths() {
-    return fs
-      .readdirSync('src')
-      .map((component) => ({ params: { component } }));
+    const file = fs.readFileSync('custom-elements.json');
+    const manifest = JSON.parse(file.toString()) as Package;
+
+    return manifest.modules
+      .filter((module) => !module.path.startsWith('src/icon'))
+      .map((module) => {
+        if (!module.exports)
+          throw new Error(`Module has no export: ${module.path}`);
+
+        const component = module.declarations?.[0] as CustomElement;
+
+        if (!component.tagName || !component.tagName.startsWith('tap')) {
+          throw new Error(`Module has no custom element: ${module.path}`);
+        }
+
+        let content = '';
+
+        content += `# ${component.name} \n`;
+
+        if (component.summary) {
+          content += `${component.summary}`;
+        }
+
+        return {
+          params: {
+            component: component.tagName,
+          },
+          content,
+        };
+      });
   },
 };
