52ed94f67c4a8ea6f21dafa54b97a7f45e4e8eba
majidsajadi <majidsajadi93@gmail.com>
2024-05-08 19:53:04 +0330

    docs: update github url

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -42,7 +42,7 @@ export default defineConfig({
     ],
 
     socialLinks: [
-      { icon: 'github', link: 'https://github.com/vuejs/vitepress' },
+      { icon: 'github', link: 'https://github.com/Tap30/web-components' },
     ],
   },
 });
