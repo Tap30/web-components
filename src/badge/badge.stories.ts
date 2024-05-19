defc1cf3742bbee5c5736a7b1d6596a0a7d2b623
Amir Hossein Alibakhshi <amir78729@gmail.com>
2024-05-19 19:49:42 +0330

    style: fix eslint for badge

--- src/badge/badge.stories.ts
+++ src/badge/badge.stories.ts
@@ -1,37 +1,37 @@
-import { html, TemplateResult } from "lit";
-import "./index.js";
-import { BadgePriority, BadgeType, BadgeValue, BadgeVariant } from "./types";
+import { html, TemplateResult } from 'lit';
+import './index.js';
+import { BadgePriority, BadgeType, BadgeValue, BadgeVariant } from './types';
 
-const BadgeTypes = ["pill", "numeral", "dot"]
-const BadgeVariants = ["success", "error", "info", "inverse", "warning"]
-const BadgePriorities = ["high", "low"]
+const BadgeTypes = ['pill', 'numeral', 'dot'];
+const BadgeVariants = ['success', 'error', 'info', 'inverse', 'warning'];
+const BadgePriorities = ['high', 'low'];
 
 export default {
-  title: "Badge",
-  component: "tap-badge",
+  title: 'Badge',
+  component: 'tap-badge',
   argTypes: {
     value: {
-      control: "text",
-      description: "Badge Value",
+      control: 'text',
+      description: 'Badge Value',
     },
     type: {
       options: BadgeTypes,
-      control: { type: "inline-radio" },
-      description: "Badge Type",
+      control: { type: 'inline-radio' },
+      description: 'Badge Type',
     },
     variant: {
       options: BadgeVariants,
-      control: { type: "select" },
-      description: "Badge Variant",
+      control: { type: 'select' },
+      description: 'Badge Variant',
     },
     priority: {
       options: BadgePriorities,
-      control: { type: "inline-radio" },
-      description: "Badge Priority",
+      control: { type: 'inline-radio' },
+      description: 'Badge Priority',
     },
     leadingIcon: {
-      description: "Should have leading icon?",
-      control: { type: "boolean" },
+      description: 'Should have leading icon?',
+      control: { type: 'boolean' },
     },
   },
 };
@@ -50,7 +50,13 @@ interface ArgTypes {
   leadingIcon: boolean;
 }
 
-const Template: Story<ArgTypes> = ({value, variant, type, priority, leadingIcon}: ArgTypes) => html`
+const Template: Story<ArgTypes> = ({
+  value,
+  variant,
+  type,
+  priority,
+  leadingIcon,
+}: ArgTypes) => html`
   <tap-badge
     value=${value}
     type=${type}
@@ -63,9 +69,9 @@ const Template: Story<ArgTypes> = ({value, variant, type, priority, leadingIcon}
 export const Badge = Template.bind({});
 
 Badge.args = {
-  value: "نشان",
-  type: "pill",
-  variant: "info",
-  priority: "high",
-  leadingIcon: false
+  value: 'نشان',
+  type: 'pill',
+  variant: 'info',
+  priority: 'high',
+  leadingIcon: false,
 };
--- src/badge/types.ts
+++ src/badge/types.ts
@@ -1,4 +1,4 @@
 export type BadgeValue = string | number;
-export type BadgeType = "pill" | "numeral" | "dot";
-export type BadgeVariant = "success" | "error" | "info" | "warning" | "inverse";
-export type BadgePriority = "high" | "low";
+export type BadgeType = 'pill' | 'numeral' | 'dot';
+export type BadgeVariant = 'success' | 'error' | 'info' | 'warning' | 'inverse';
+export type BadgePriority = 'high' | 'low';
