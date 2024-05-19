fdfa8110a2639dfcc6920103b50b5f9af5d3c7a9
Amir Hossein Alibakhshi <amir78729@gmail.com>
2024-05-19 20:08:53 +0330

    docs: add JSDoc to badge component

--- src/badge-wrapper/badge-wrapper.ts
+++ src/badge-wrapper/badge-wrapper.ts
@@ -3,9 +3,9 @@ import { html, LitElement } from "lit";
 export class BadgeWrapper extends LitElement {
   render() {
     return html`
-      <span class="wrapper">
+      <span class="wrapper" part="wrapper">
         <slot></slot>
-        <slot class="badge" name="badge"></slot>
+        <slot class="badge" name="badge" part="badge"></slot>
       </span>
     `;
   }
