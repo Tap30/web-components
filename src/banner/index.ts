e1dfbcdf76926a95e2a26cad9b3dc18cbebdc93f
Amir Hossein Alibakhshi <amir78729@gmail.com>
2024-05-20 09:05:18 +0330

    style: fix eslint

--- src/banner/index.ts
+++ src/banner/index.ts
@@ -1,14 +1,14 @@
-import { customElement } from "lit/decorators.js";
-import { Banner } from "./banner.js";
-import styles from "./banner.style.js";
+import { customElement } from 'lit/decorators.js';
+import { Banner } from './banner.js';
+import styles from './banner.style.js';
 
-@customElement("tap-banner")
+@customElement('tap-banner')
 export class TapBanner extends Banner {
   static readonly styles = [styles];
 }
 
 declare global {
   interface HTMLElementTagNameMap {
-    "tap-banner": TapBanner;
+    'tap-banner': TapBanner;
   }
 }
