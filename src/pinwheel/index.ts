777c10a02f5a5f2f9357b2a529ff874283ed9541
mohammadreza pakzadian <pakzadianmrp@gmail.com>
2024-05-20 11:10:33 +0330

    doc: update JSDoc

--- src/pinwheel/index.ts
+++ src/pinwheel/index.ts
@@ -11,26 +11,26 @@ import styles from "./pinwheel.style";
  *
  * @summary A pinwheel component for selecting items by scrolling through a list.
  *
- * @prop {string[]} items - An array of strings that contains pinwheel items.
+ * @prop {string[]} `items` - An array of strings that contains pinwheel items.
  *
- * @csspart pinwheel - The container that wraps the pinwheel component.
- * @csspart pinwheel-item - The item that rendered in pinwheel.
+ * @csspart `pinwheel` - The container that wraps the pinwheel component.
+ * @csspart `pinwheel-item` - The item that rendered in pinwheel.
  *
- * @cssprop [--tap-pinwheel-vertical-padding=--tap-sys-spacing-0]
- * @cssprop [-tap-pinwheel-horizontal-padding=--tap-sys-spacing-6]
+ * @cssprop [`--tap-pinwheel-vertical-padding`=`--tap-sys-spacing-0`]
+ * @cssprop [`--tap-pinwheel-horizontal-padding`=`--tap-sys-spacing-6`]
  *
- * @cssprop [--tap-pinwheel-item-height=--tap-sys-spacing-11]
+ * @cssprop [`--tap-pinwheel-item-height`=`--tap-sys-spacing-11`]
  *
- * @cssprop [--tap-font-family=--tap-sys-font-family]
- * @cssprop [--tap-pinwheel-typography-body-md-height=--tap-sys-typography-body-md-height]
- * @cssprop [--tap-pinwheel-typography-body-md-size=--tap-sys-typography-body-md-size]
- * @cssprop [--tap-pinwheel-typography-body-md-weight=--tap-sys-typography-body-md-weight]
- * @cssprop [--tap-pinwheel-item-color=--tap-sys-color-content-tertiary]
+ * @cssprop [`--tap-font-family`=`--tap-sys-font-family`]
+ * @cssprop [`--tap-pinwheel-typography-body-md-height`=`--tap-sys-typography-body-md-height`]
+ * @cssprop [`--tap-pinwheel-typography-body-md-size`=`--tap-sys-typography-body-md-size`]
+ * @cssprop [`--tap-pinwheel-typography-body-md-weight`=`--tap-sys-typography-body-md-weight`]
+ * @cssprop [`--tap-pinwheel-item-color`=`--tap-sys-color-content-tertiary`]
  *
- * @cssprop [--tap-pinwheel-typography-label-md-height=--tap-sys-typography-label-md-height]
- * @cssprop [--tap-pinwheel-typography-label-md-size=--tap-sys-typography-label-md-size]
- * @cssprop [--tap-pinwheel-typography-label-md-weight=--tap-sys-typography-label-md-weight]
- * @cssprop [--tap-pinwheel-active-item-color=--tap-sys-color-content-primary]
+ * @cssprop [`--tap-pinwheel-typography-label-md-height`=`--tap-sys-typography-label-md-height`]
+ * @cssprop [`--tap-pinwheel-typography-label-md-size`=`--tap-sys-typography-label-md-size`]
+ * @cssprop [`--tap-pinwheel-typography-label-md-weight`=`--tap-sys-typography-label-md-weight`]
+ * @cssprop [`--tap-pinwheel-active-item-color`=`--tap-sys-color-content-primary`]
  *
  */
 @customElement("tap-pinwheel")
