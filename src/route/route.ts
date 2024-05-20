e553825001d4662008f56b7b6cec95e3de4b3dc9
Maryam Rahimi <maryam.rahimie94@gmail.com>
2024-05-20 17:04:12 +0330

    feat: route base created

---
+++ src/route/route.ts
@@ -0,0 +1,39 @@
+import {html, LitElement, PropertyValues} from "lit";
+import {property} from "lit/decorators.js";
+import {TapRow} from "../row";
+
+export class Route extends TapRow {
+  @property({reflect: true, attribute: "leading-icon"}) leadingIcon: "circle" | "square" | "plus" = "circle";
+
+  @property({reflect: true}) ordinal: "first" | "middle" | "last" = "first";
+
+  @property({type: Boolean, reflect: true}) editable: boolean = false;
+
+  private renderLeadingIcon() {
+    switch (this.leadingIcon) {
+      case 'circle':
+        return html`
+          <tap-icon-dot-fill slot="leading"></tap-icon-dot-fill>`;
+      case 'square':
+        return html`
+          <tap-icon-square-fill slot="leading"></tap-icon-square-fill>`;
+      case 'plus':
+        return html`
+          <tap-icon-plus-fill slot="leading"></tap-icon-plus-fill>`;
+    }
+  }
+
+  private renderAction() {
+    return this.editable ? html`<tap-icon-pencil-line-fill slot="trailing"></tap-icon-pencil-line-fill>` : '';
+  }
+
+  protected render(): unknown {
+    return html`
+      <tap-row size=${this.size}>
+        ${this.renderLeadingIcon()}
+        ${this.renderAction()}
+        <slot name="content" slot="content"></slot>
+      </tap-row>
+    `;
+  }
+}
