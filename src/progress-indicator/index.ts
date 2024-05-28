200ad800c767161c15d5511d2a135d92b1b79688
Amir Hossein Alibakhshi <amir78729@gmail.com>
2024-05-28 14:15:57 +0330

    docs: add jsdoc

--- src/progress-indicator/index.ts
+++ src/progress-indicator/index.ts
@@ -2,6 +2,29 @@ import { customElement } from 'lit/decorators.js';
 import { ProgressIndicator } from './progress-indicator.js';
 import styles from './progress-indicator.style.js';
 
+/**
+ * ### Example
+ *
+ *
+ * ##### Simple
+ *
+ * ```html
+ * <tap-progress-indicator max="4" current="2"></tap-progress-indicator>
+ * ```
+ *
+ * @summary A progress indicator component displaying multiple steps.
+ *
+ * @prop {`number`} [`max`=`2`] - The total number of steps.
+ * @prop {`number`} [`current`=`0`] - The current step index.
+ *
+ * @csspart [`progressbar`] - The container for the progress bar.
+ * @csspart [`step`] - Each individual step in the progress bar.
+ *
+ * @cssprop [`--tap-progress-indicator-progressbar-color`=`--tap-sys-spacing-3`] - The gap between steps.
+ * @cssprop [`--tap-progress-indicator-step-height`=`--tap-sys-spacing-3`] - The height of each step.
+ * @cssprop [`--tap-progress-indicator-step-background-color`=`--tap-sys-color-surface-tertiary`] - The background color of each step.
+ * @cssprop [`--tap-progress-indicator-active-step-background-color`=`--tap-sys-color-border-inverse-primary`] - The background color of active steps.
+ */
 @customElement('tap-progress-indicator')
 export class TapProgressIndicator extends ProgressIndicator {
   static readonly styles = [styles];
