0360f0cd000ff8e4dcb6672a21aece219cf313ed
Amir Hossein Alibakhshi <amir78729@gmail.com>
2024-05-28 15:38:37 +0330

    style: fix eslint

--- src/segmented-button-group/index.ts
+++ src/segmented-button-group/index.ts
@@ -1,14 +1,14 @@
-import { customElement } from "lit/decorators.js";
-import { SegmentedButtonGroup } from "./segmented-button-group";
-import styles from "./segmented-button-group.style";
+import { customElement } from 'lit/decorators.js';
+import { SegmentedButtonGroup } from './segmented-button-group';
+import styles from './segmented-button-group.style';
 
-@customElement("tap-segmented-button-group")
+@customElement('tap-segmented-button-group')
 export class TapSegmentedButtonGroup extends SegmentedButtonGroup {
   static readonly styles = [styles];
 }
 
 declare global {
   interface HTMLElementTagNameMap {
-    "tap-segmented-button-group": TapSegmentedButtonGroup;
+    'tap-segmented-button-group': TapSegmentedButtonGroup;
   }
 }
