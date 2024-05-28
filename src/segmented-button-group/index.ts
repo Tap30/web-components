c756231b00180fe45b8dbaace757ae115814218e
Amir Hossein Alibakhshi <amir78729@gmail.com>
2024-05-28 16:28:07 +0330

    docs: add jsdoc

--- src/segmented-button-group/index.ts
+++ src/segmented-button-group/index.ts
@@ -2,6 +2,46 @@ import { customElement } from 'lit/decorators.js';
 import { SegmentedButtonGroup } from './segmented-button-group';
 import styles from './segmented-button-group.style';
 
+/**
+ * ### Example
+ *
+ *
+ * ##### Simple
+ *
+ * ```html
+ * <tap-segmented-button-group>
+ *   <tap-segmented-button>Button 1</tap-segmented-button>
+ *   <tap-segmented-button>Button 2</tap-segmented-button>
+ *   <tap-segmented-button>Button 3</tap-segmented-button>
+ * </tap-segmented-button-group>
+ * ```
+ *
+ * ##### With Sizes
+ *
+ * ```html
+ * <tap-segmented-button-group size="sm">
+ *   <tap-segmented-button>Small Button 1</tap-segmented-button>
+ *   <tap-segmented-button>Small Button 2</tap-segmented-button>
+ * </tap-segmented-button-group>
+ *
+ * <tap-segmented-button-group size="md">
+ *   <tap-segmented-button>Medium Button 1</tap-segmented-button>
+ *   <tap-segmented-button>Medium Button 2</tap-segmented-button>
+ * </tap-segmented-button-group>
+ * ```
+ *
+ * @summary A group of segmented buttons.
+ *
+ * @prop {`'sm'` \| `'md'`} [`size`=`'md'`] - The size of the segmented button group.
+ *
+ * @csspart [`button-group`] - The main container for the button group.
+ *
+ * @cssprop [`--tap-segmented-button-group-color-surface`=`--tap-sys-color-surface-secondary`] - The background color of the button group.
+ * @cssprop [`--tap-segmented-button-group-border-radius`=`--tap-sys-radius-full`] - The border radius of the button group.
+ * @cssprop [`--tap-segmented-button-group-padding`=`--tap-sys-spacing-3`] - The padding inside the button group.
+ * @cssprop [`--tap-segmented-button-group-sm-height`=`--tap-sys-spacing-10`] - The height of the small size button group.
+ * @cssprop [`--tap-segmented-button-group-md-height`=`--tap-sys-spacing-11`] - The height of the medium size button group.
+ */
 @customElement('tap-segmented-button-group')
 export class TapSegmentedButtonGroup extends SegmentedButtonGroup {
   static readonly styles = [styles];
--- src/segmented-button/index.ts
+++ src/segmented-button/index.ts
@@ -2,6 +2,43 @@ import { customElement } from 'lit/decorators.js';
 import { SegmentedButton } from './segmented-button';
 import styles from './segmented-button.style';
 
+/**
+ * ### Example
+ *
+ *
+ * ##### Simple
+ *
+ * ```html
+ * <tap-segmented-button>Button</tap-segmented-button>
+ * ```
+ *
+ * ##### Selected
+ *
+ * ```html
+ * <tap-segmented-button selected>Selected Button</tap-segmented-button>
+ * ```
+ *
+ * ##### Disabled
+ *
+ * ```html
+ * <tap-segmented-button disabled>Disabled Button</tap-segmented-button>
+ * ```
+ *
+ * @summary A segmented button component.
+ *
+ * @prop {`boolean`} [`selected`=`false`] - Indicates whether the button is selected.
+ * @prop {`boolean`} [`disabled`=`false`] - Indicates whether the button is disabled.
+ *
+ * @csspart [`button`] - The main container for the button.
+ *
+ * @cssprop [`--tap-font-family`=`--tap-sys-font-family`] - The font family used in the button.
+ * @cssprop [`--tap-segmented-button-border-radius`=`--tap-sys-radius-full`] - The border radius of the button.
+ * @cssprop [`--tap-segmented-button-color`=`--tap-sys-color-content-primary`] - The text color of the button.
+ * @cssprop [`--tap-segmented-button-line-height`=`--tap-sys-typography-label-sm-height`] - The line height of the button text.
+ * @cssprop [`--tap-segmented-button-font-size`=`--tap-sys-typography-label-sm-size`] - The font size of the button text.
+ * @cssprop [`--tap-segmented-button-font-weight`=`--tap-sys-typography-label-sm-weight`] - The font weight of the button text.
+ * @cssprop [`--tap-segmented-button-selected-background-color`=`--tap-sys-color-surface-primary`] - The background color of the selected button.
+ */
 @customElement('tap-segmented-button')
 export class TapSegmentedButton extends SegmentedButton {
   static readonly styles = [styles];
