e34ec5fdc76c10f01c8e7a2111ac7b54576207f4
Mahdieh Abdollahian <Mahdieh.abdollahian@Tapsi.cab>
2024-05-29 13:53:10 +0330

    fix: revert jsdoc and linter changes

--- src/spinner/spinner.style.ts
+++ src/spinner/spinner.style.ts
@@ -1,4 +1,4 @@
-import { css } from "lit";
+import { css } from 'lit';
 
 export default css`
   :host {
@@ -6,27 +6,31 @@ export default css`
   }
 
   .spinner {
-    width: 20px;
-    height: 20px;
-    padding: 2px;
+    width: var(--tap-spinner-size, var(--tap-sys-spacing-8));
+    height: var(--tap-spinner-size, var(--tap-sys-spacing-8));
+    padding: var(--tap-spinner-padding, var(--tap-sys-spacing-2));
     stroke-linecap: round;
   }
 
   .rotating {
     transform-origin: 300px 300px;
-    animation : rotate 1s linear infinite;
+    animation: rotate 1s linear infinite;
   }
 
   .primary {
-    color: var(--tap-sys-color-surface-black);
+    color: var(--tap-spinner-color-primary, var(--tap-sys-color-surface-black));
   }
 
   .inverse {
-    color: var(--tap-sys-color-surface-white);
+    color: var(--tap-spinner-color-inverse, var(--tap-sys-color-surface-white));
   }
 
   @keyframes rotate {
-    from {transform: rotate(105deg);}
-    to {transform: rotate(465deg);}
+    from {
+      transform: rotate(105deg);
+    }
+    to {
+      transform: rotate(465deg);
+    }
   }
 `;
