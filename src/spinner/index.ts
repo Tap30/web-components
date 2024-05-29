bc56c282197c7746d875c8d5f40ea1ae42d4d0a7
Mahdieh Abdollahian <Mahdieh.abdollahian@Tapsi.cab>
2024-05-29 13:43:29 +0330

    fix: fix conflicts

--- src/spinner/index.ts
+++ src/spinner/index.ts
@@ -2,6 +2,27 @@ import { customElement } from "lit/decorators.js";
 import { Spinner } from "./spinner";
 import styles from "./spinner.style";
 
+/** 
+* ### Example
+* 
+* 
+* ##### Simple
+*
+* ```html
+* <tap-spinner />
+* ```
+*
+* ##### Variant
+*
+* ```html
+* <tap-spinner variant="inverse" />
+* ```
+*
+* @summary Display spinner.
+*
+* @prop {string} [variant='primary'] - Specifies the spinner color. Acceptable values are [primary , inverse] .
+*/
+
 @customElement("tap-spinner")
 export class TapSpinner extends Spinner {
   static readonly styles = [styles];
