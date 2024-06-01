a525d2eb4bfc9f82d6eed75ff2f728585ef49387
mohammadreza pakzadian <pakzadianmrp@gmail.com>
2024-06-01 18:29:49 +0330

    fix: update timeout type in debounce function

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,5 +1,5 @@
 export function debounce<T>(func: (...args: T[]) => void, delay: number) {
-  let timeout: number | null = null;
+  let timeout: ReturnType<typeof setTimeout> | null = null;
   return (...args: T[]) => {
     if (timeout)
       clearTimeout(timeout)
