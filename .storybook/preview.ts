71561bf2e8794512a2cec8a6b4e016c80c7c9d01
Amir Hossein Alibakhshi <amir78729@gmail.com>
2024-06-04 01:26:00 +0330

    fix: delete icon stories

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,11 +1,34 @@
 import type { Preview } from "@storybook/web-components";
-
+import { setCustomElementsManifest } from '@storybook/web-components';
 import "../styles/font.css";
 import "../styles/theme.css";
+import DocTemplate from "./DocTemplate.mdx"
+// setCustomElementsManifest('../dist/custom-element.json')
 
 const preview: Preview = {
   parameters: {
     controls: { expanded: true },
+    docs: {
+      page: DocTemplate,
+      toc: {
+        ignoreSelector: '#primary',
+        disable: false,
+      },
+    },
+    backgrounds: {
+      default: 'white',
+      values: [
+        {
+          name: 'white',
+          value: '#ffffff',
+        },
+        {
+          name: 'black',
+          value: '#000000',
+        },
+      ], // TODO: add other tokens from design system
+    }
   },
 };
+
 export default preview;
