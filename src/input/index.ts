360f3cd7e627dec4563e4ea43bda19de0498e63e
Negarnbkhti42 <norob35@gmail.com>
2024-06-05 16:34:35 +0330

    change: remove element wrapper for abstract class

--- src/input/index.ts
+++ src/input/index.ts
@@ -1,14 +1,6 @@
-import { customElement } from 'lit/decorators.js';
 import { Input } from './input';
 import styles from './input.style';
 
-@customElement('tap-input')
 export abstract class TapInput extends Input {
     static readonly styles = [styles];
-}
-
-declare global {
-    interface HTMLElementTagNameMap {
-        'tap-input': TapInput;
-    }
 }
