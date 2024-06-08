831cb74b07d6032c1f089e1b5051f84d56d46a87
majidsajadi <majidsajadi93@gmail.com>
2024-06-08 14:32:28 +0330

    fix: remove unused params in avatar story

--- src/avatar/avatar.stories.ts
+++ src/avatar/avatar.stories.ts
@@ -34,7 +34,7 @@ interface ArgTypes {
   loading: 'lazy' | 'eager';
 }
 
-const Template: Story<ArgTypes> = ({ image, size, loading }: ArgTypes) => html`
+const Template: Story<ArgTypes> = ({}: ArgTypes) => html`
   <tap-avatar image="https://picsum.photos/100">
     <svg fill="#666" viewBox="0 0 24 24">
       <path
