4070963030dc1c567a2cf347d89854219962c174
Amir Hossein Alibakhshi <amir78729@gmail.com>
2024-06-22 16:57:33 +0330

    fix: update doc

--- docs/components/[component].paths.ts
+++ docs/components/[component].paths.ts
@@ -68,7 +68,6 @@ export default {
           component.cssParts.forEach((cssPart) => {
             content += `| \`${cssPart.name}\` | ${cssPart.description} |\n`;
           });
-
           content += `\n > Check [this link](/references/css-parts.html#${component.tagName}) to learn how to use CSS parts for ${getPageTitle(component.tagName).replace('Tap ', '')}.\n`;
         }
 
