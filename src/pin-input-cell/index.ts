dc306901259367e3f3d89fc1f723ff3896d39e12
Hossein Nasiri <hossein.nasiri.sovari@gmail.com>
2024-07-08 11:49:41 +0330

    fix: convert events to concrete event classes

--- src/pin-input-cell/index.ts
+++ src/pin-input-cell/index.ts
@@ -1,6 +1,8 @@
 import { customElement } from 'lit/decorators.js';
 import { PinInputCell } from './pin-input-cell.js';
 import styles from './pin-input-cell.style.js';
+export * from './events.js';
+export * from './constants.js';
 
 @customElement('tap-pin-input-cell')
 export class TapPinInputCell extends PinInputCell {
