f29c5a423d86855c7dae279b3f40c9a31c52e4b5
zeynab.nasiri <zeinab.nasiri1996@gmail.com>
2024-08-04 10:41:58 +0330

    fix: animations and container style

--- src/bottom-sheet/bottom-sheet.style.ts
+++ src/bottom-sheet/bottom-sheet.style.ts
@@ -2,10 +2,10 @@ import { css } from 'lit';
 
 export default css`
   .bottom-sheet {
-    // transform: translateY(100vh);
     position: absolute;
-    bottom: 0;
-    transition: transform 0.3s cubic-bezier(0, 1, 0, 1);
+    bottom: var(--tap-bottom-sheet-bottom, 0);
+    height: 90vh;
+    transition: 0.3s;
     direction: rtl;
     max-width: 440px;
     width: 100%;
@@ -13,11 +13,13 @@ export default css`
       --tap-bottom-sheet-background,
       var(--tap-sys-color-surface-primary)
     );
-    z-index: 2;
     box-sizing: border-box;
     font-family: var(--tap-font-family, var(--tap-sys-font-family));
     border-radius: 20px 20px 0 0;
-    transition: 1s;
+    border: 1px solid var(--tap-palette-gray-200);
+    border-bottom: 0;
+    transform: translate(-50%, 0);
+    left: 50%;
   }
 
   .bottom-sheet-dimmer {
@@ -29,21 +31,10 @@ export default css`
     transform: translateX(-50%);
     background-color: #33323b4b;
     animation: fade-in 0.3s ease-in-out;
-
-    @keyframes fade-in {
-      from {
-        opacity: 0;
-      }
-      to {
-        opacity: 1;
-      }
-    }
   }
 
   .bottom-sheet-body {
     background-color: inherit;
-    overflow-y: hidden;
-    height: 100%;
   }
 
   .bottom-sheet-header {
@@ -80,39 +71,43 @@ export default css`
     position: absolute;
     top: 8px;
     background-color: rgba(0, 0, 0, 0.12);
-    z-index: 1;
     right: 50%;
     transform: translateX(50%);
     cursor: row-resize;
   }
 
   .close {
-    animation: slide-down 1s cubic-bezier(0, 1, 0, 1) forwards;
+    animation: slide-down 0.3s ease-in forwards;
   }
 
   .open {
-    animation: slide-up 0.3s cubic-bezier(0, 1, 0, 1) forwards;
+    animation: slide-up 0.3s ease-in forwards;
   }
 
   @keyframes slide-down {
-    0% {
-      translateY(0)
-    }
-    70% {
-      opacity: 1;
+    from {
+      transform: translate(-50%, 0);
     }
     to {
-      transform: translateY(100%);
-      opacity: 0;
+      transform: translate(-50%, 100%);
     }
   }
 
   @keyframes slide-up {
     from {
-      transform: translateY(0);
+      transform: translate(-50%, 100%);
     }
     to {
-      transform: translateY(-70vh);
+      transform: translate(-50%, -0);
+    }
+  }
+
+  @keyframes fade-in {
+    from {
+      opacity: 0;
+    }
+    to {
+      opacity: 1;
     }
   }
 `;
