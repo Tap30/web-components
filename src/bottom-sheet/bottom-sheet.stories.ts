4c38fbe027de54aab807707751d63a44c8a462ef
zeynab.nasiri <zeinab.nasiri1996@gmail.com>
2024-08-04 10:41:00 +0330

    feat: add storybook's argTypes

--- src/bottom-sheet/bottom-sheet.stories.ts
+++ src/bottom-sheet/bottom-sheet.stories.ts
@@ -1,4 +1,5 @@
 import { html, TemplateResult } from 'lit';
+import { Meta } from '@storybook/web-components';
 import './index.js';
 import '../tooltip';
 
@@ -8,7 +9,49 @@ export default {
   parameters: {
     layout: 'fullscreen',
   },
-};
+  argTypes: {
+    isOpen: {
+      control: 'boolean',
+      description:
+        'Controls the visibility of the bottom sheet. If true, the bottom sheet is visible.',
+    },
+    isDismissible: {
+      control: 'boolean',
+      description:
+        'Determines whether the bottom sheet can be dismissed by the user. If true, a close button is displayed, allowing the bottom sheet to be closed.',
+    },
+    hasDimmer: {
+      control: 'boolean',
+      description: 'Controls the presence of a dimmer overlay.',
+    },
+    title: {
+      control: 'text',
+      description:
+        'Specifies the title displayed in the header of the bottom sheet.',
+    },
+    isExpanded: {
+      control: 'boolean',
+      description:
+        'If true, the bottom sheet expands to 90% of the viewport height (90vh).',
+    },
+    showGrabber: {
+      control: 'boolean',
+      description: 'Controls the visibility of the grabber element.',
+    },
+  },
+  decorators: [
+    (Story: () => TemplateResult) => {
+      return html`
+        <style>
+          body {
+            overflow: hidden;
+          }
+        </style>
+        ${Story()}
+      `;
+    },
+  ],
+} as Meta;
 
 interface Story<T> {
   (args: T): TemplateResult;
@@ -20,13 +63,18 @@ interface ArgTypes {
   isOpen?: boolean;
   disappear?: boolean;
   isDismissible?: boolean;
+  hasDimmer?: boolean;
   title?: string;
   isExpanded?: boolean;
   showGrabber?: boolean;
 }
 
-const Template: Story<ArgTypes> = ({ title, isOpen }) => {
-  return html` <tap-bottom-sheet title=${title} ?is-open=${isOpen}>
+const Template: Story<ArgTypes> = ({ title, isOpen, hasDimmer }) => {
+  return html` <tap-bottom-sheet
+    title=${title}
+    ?is-open=${isOpen}
+    ?has-dimmer=${hasDimmer}
+  >
     <div slot="bottom-sheet-body" style="">This is body!</div>
   </tap-bottom-sheet>`;
 };
@@ -34,5 +82,6 @@ const Template: Story<ArgTypes> = ({ title, isOpen }) => {
 export const Simple = Template.bind({});
 Simple.args = {
   title: 'عنوان',
-  isOpen: false,
+  isOpen: true,
+  hasDimmer: true,
 };
