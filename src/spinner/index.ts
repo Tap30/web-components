8b9fcc67eac636dc1c3aad7f8084fcee8ab0a327
Amir Hossein Alibakhshi <amir78729@gmail.com>
2024-08-12 16:58:35 +0330

    docs: update spinners JSDoc

--- src/spinner/index.ts
+++ src/spinner/index.ts
@@ -3,33 +3,19 @@ import { Spinner } from './spinner';
 import styles from './spinner.style';
 
 /**
- * ### Example
- *
- *
- * ##### Simple
- *
- * ```html
- * <tap-spinner />
- * ```
- *
- * ##### Variant
- *
- * ```html
- * <tap-spinner variant="inverse" />
- * ```
- *
  * @summary Display spinner.
  *
- * @prop {`'default'` \| `'primary'` \| `'inverse'`} [`variant`=`'primary'`] - Specifies the spinner color. Acceptable values are `primary` and `inverse`.
+ * @prop {'default'|'primary'|'inverse'} [variant='primary'] - Specifies the spinner color. Acceptable values are `primary` and `inverse`.
+ * @prop {'small' | 'medium' | 'large'} [size='medium'] - The size of the spinner.
  *
  * @csspart `svg` - The svg tag that represents spinner component.
  *
- * @cssprop [`--tap-spinner-color-primary`=`--tap-sys-color-surface-black`]
- * @cssprop [`--tap-spinner-color-inverse`=`--tap-sys-color-surface-white`]
- * @cssprop [`--tap-spinner-sm-size`=`--tap-sys-spacing-7`]
- * @cssprop [`--tap-spinner-md-size`=`--tap-sys-spacing-8`]
- * @cssprop [`--tap-spinner-lg-size`=`--tap-sys-spacing-10`]
- * @cssprop [`--tap-spinner-padding`=`--tap-sys-spacing-2`]
+ * @cssprop [--tap-spinner-color-primary=--tap-sys-color-surface-black]
+ * @cssprop [--tap-spinner-color-inverse=--tap-sys-color-surface-white]
+ * @cssprop [--tap-spinner-sm-size=--tap-sys-spacing-7]
+ * @cssprop [--tap-spinner-md-size=--tap-sys-spacing-8]
+ * @cssprop [--tap-spinner-lg-size=--tap-sys-spacing-10]
+ * @cssprop [--tap-spinner-padding=--tap-sys-spacing-2]
  */
 
 @customElement('tap-spinner')
