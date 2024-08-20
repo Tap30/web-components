3cd4b970f1a2d51bb2d8aa3712a42aeb10652a96
sara parsa <sara.pe9820@gmail.com>
2024-08-20 19:41:53 +0330

    Feat: add chip component (#169)
    * Feat: add chip component
    
    * refactor: pass the size property to the chip, add select mode and fullwidth property
    
    * refactor: add the chip and chip group components to the doc
    
    * refactor: change styles of the chip component
    
    ---------
    
    Co-authored-by: Sara.Parsaee <Sara.Parsaee@Tapsi.cab>

---
+++ src/chip/chip.ts
@@ -0,0 +1,44 @@
+import { LitElement, html, nothing } from 'lit';
+import { property } from 'lit/decorators.js';
+
+export class Chip extends LitElement {
+  static readonly shadowRootOptions = {
+    ...LitElement.shadowRootOptions,
+    delegatesFocus: true,
+  };
+
+  @property({ reflect: true, type: Boolean }) selected = false;
+
+  @property({ type: Boolean }) disabled = false;
+
+  @property({ reflect: true, type: Boolean }) hasIcon = false;
+
+  @property({ reflect: true }) size: 'sm' | 'md' = 'md';
+
+  private handleClick() {
+    this.dispatchEvent(
+      new Event('chip-click', {
+        bubbles: true,
+        composed: true,
+      }),
+    );
+  }
+
+  render() {
+    const { ariaLabel } = this;
+
+    return html`
+      <button
+        class="chip"        
+        ?disabled=${this.disabled}
+        tabindex="${this.disabled ? '-1' : '0'}"
+        @click="${this.handleClick}"
+        aria-label=${ariaLabel || nothing}
+        aria-pressed=${this.selected}
+      >
+        <slot></slot>
+        ${this.hasIcon ? html`<slot name="icon"></slot>` : nothing}
+      </button>
+    `;
+  }
+}
