14d86c87fc3de1c2f1baf68a3247f0fb608555d8
majidsajadi <majidsajadi93@gmail.com>
2024-08-20 19:48:14 +0330

    fix: tap icon from tap icon button

--- src/icon-button/icon-button.ts
+++ src/icon-button/icon-button.ts
@@ -1,31 +1,3 @@
-import { queryAssignedElements } from 'lit/decorators.js';
 import { BaseButton } from '../base-button';
-import { PropertyValues } from 'lit';
-import { TapIcon } from '../icon';
 
-export class IconButton extends BaseButton {
-  @queryAssignedElements() private icon!: TapIcon[];
-
-  protected updated(_changedProperties: PropertyValues) {
-    super.updated(_changedProperties);
-    const isSizeChanged = !!_changedProperties.get('size');
-    if (isSizeChanged) this.updateIconSize();
-  }
-
-  private updateIconSize = () => {
-    if (this.icon[0]) {
-      this.icon[0].width = this.getIconSize();
-      this.icon[0].height = this.getIconSize();
-    }
-  };
-
-  private getIconSize = () => {
-    switch (this.size) {
-      case 'small':
-        return 20;
-      case 'medium':
-      case 'large':
-        return 24;
-    }
-  };
-}
+export class IconButton extends BaseButton {}
