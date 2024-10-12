558e8d8837aab6befab18825225432528e3ff3b6
mimshins <mostafa.sh.coderino@gmail.com>
2024-10-12 16:27:37 +0330

    add logger utility

--- src/utils.ts
+++ src/utils.ts
@@ -38,3 +38,22 @@ export function redispatchEvent(element: Element, event: Event) {
 
   return dispatched;
 }
+
+export const logger = (
+  message: string,
+  scope: string,
+  type: "error" | "default" | "warning" = "default",
+) => {
+  const typesMap: Record<typeof type, Console["log"]> = {
+    /* eslint-disable no-console */
+    error: console.error,
+    default: console.log,
+    warning: console.warn,
+    /* eslint-enable no-console */
+  };
+
+  const logFn = typesMap[type];
+  const completeMessage = `[TAPSI][${scope}]: ${message}`;
+
+  logFn(completeMessage);
+};
