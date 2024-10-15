e9b84f1ee0831416d67a3c914d6a6a190453c4ac
Amir Hossein Alibakhshi <amir78729@gmail.com>
2024-10-15 10:16:38 +0330

    docs(nps): update jsdoc

--- src/nps/index.ts
+++ src/nps/index.ts
@@ -3,18 +3,18 @@ import { Nps } from "./nps";
 import styles from "./nps.style";
 
 /**
- * @summary A slider component for NPS Rating
+ * @summary A slider component for NPS rating
  *
- * @prop {number} [min=0] - The min value.
- * @prop {number} [max=10] - The max value.
- * @prop {number} [value] - The current value.
+ * @prop {number} [min=0] - The minimum value for the slider.
+ * @prop {number} [max=10] - The maximum value for the slider.
+ * @prop {number} value - The current value of the slider.
  *
- * @fires nps-change - Fired when the value of the NPS rate changes.
+ * @fires npschange - Fired when the NPS rating value changes.
  *
- * @csspart [nps-container] - The container of the component
- * @csspart [nps-dot] - The dot of the component
- * @csspart [nps-label] - The label of the component
- * @csspart [nps-rate] - The rate element of the component
+ * @csspart [nps-container] - The container that wraps the component.
+ * @csspart [nps-dot] - The dot that represents the selected value on the slider.
+ * @csspart [nps-label] - The label displaying the description or value of the NPS.
+ * @csspart [nps-rate] - The element that shows the selected rate.
  */
 @customElement("tap-nps")
 export class TapNps extends Nps {
