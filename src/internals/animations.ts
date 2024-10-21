8757e76e5e930ecc90cf82ae3ce7b92b3b7e1e21
mimshins <mostafa.sh.coderino@gmail.com>
2024-10-21 18:13:08 +0330

    add internals

---
+++ src/internals/animations.ts
@@ -0,0 +1,9 @@
+export const EASING = {
+  LINEAR: "linear",
+  STANDARD: "cubic-bezier(0.2, 0, 0, 1)",
+  STANDARD_ACCELERATE: "cubic-bezier(.3,0,1,1)",
+  STANDARD_DECELERATE: "cubic-bezier(0,0,0,1)",
+  EMPHASIZED: "cubic-bezier(.3,0,0,1)",
+  EMPHASIZED_ACCELERATE: "cubic-bezier(.3,0,.8,.15)",
+  EMPHASIZED_DECELERATE: "cubic-bezier(.05,.7,.1,1)",
+} as const;
