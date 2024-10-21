0cfb624a9c1f62786548cb71dea5066ebe02febe
mimshins <mostafa.sh.coderino@gmail.com>
2024-10-21 18:14:30 +0330

    update constants

--- src/bottom-sh/constants.ts
+++ src/bottom-sh/constants.ts
@@ -1,18 +1,5 @@
-export const BASENAME = "bottom-sheet";
-
-export enum Parts {
-  ROOT = "root",
-  HEADER = "header",
-  BODY = "body",
-  ACTION_BAR = "action-bar",
-  OVERLAY = "overlay",
-  GRABBER = "grabber",
-  DISMISS = "dismiss",
-  DISMISS_ICON = "dismiss-icon",
-}
-
-export enum Slots {
-  HEADER = `${BASENAME}-header`,
-  BODY = `${BASENAME}-body`,
-  ACTION_BAR = `${BASENAME}-action-bar`,
-}
+export const Slots = {
+  HEADER: "header",
+  BODY: "body",
+  ACTION_BAR: "action-bar",
+} as const;
