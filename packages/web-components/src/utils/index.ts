46ef869536cae701a59041b6ecf217a89e064cfa
mimshins <mostafa.sh.coderino@gmail.com>
2024-11-04 17:39:28 +0330

    add repaint utils

--- packages/web-components/src/utils/index.ts
+++ packages/web-components/src/utils/index.ts
@@ -2,6 +2,7 @@ export * from "./controllers";
 export * from "./dom";
 export * from "./events";
 export * from "./mixins";
+export * from "./repaint";
 
 export { default as debounce } from "./debounce";
 export { default as kebabToCamel } from "./kebab-to-camel";
