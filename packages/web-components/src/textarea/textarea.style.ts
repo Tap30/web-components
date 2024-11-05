8463846556dce314ad5e41001d28f69569515b4e
mimshins <mostafa.sh.coderino@gmail.com>
2024-11-05 18:14:39 +0330

    update styles

--- packages/web-components/src/textarea/textarea.style.ts
+++ packages/web-components/src/textarea/textarea.style.ts
@@ -1,64 +1,57 @@
 import { css } from "lit";
 
 export default css`
+  *,
+  *::before,
+  *::after {
+    box-sizing: border-box;
+  }
+
+  [hidden] {
+    display: none !important;
+  }
+
+  :host {
+    --textarea-primary-text-color: var(--tap-sys-color-content-primary);
+    --textarea-secondary-text-color: var(--tap-sys-color-content-tertiary);
+  }
+
+  .input.disabled {
+    --textarea-primary-text-color: var(--tap-sys-color-content-disabled);
+    --textarea-secondary-text-color: var(--tap-sys-color-content-disabled);
+  }
+
   .container {
-    /* FIXME: height of the input is 104px but we dont have 104px in our tokens */
-    height: 104px;
-    padding: var(--tap-textarea-md-spacing, var(--tap-sys-spacing-5))
-      var(--tap-textarea-lg-spacing, var(--tap-sys-spacing-6));
+    height: 6.5rem;
+    padding: var(--tap-sys-spacing-5) var(--tap-sys-spacing-6);
+
     align-items: flex-start;
   }
 
   .input {
     border: 0;
     outline: none;
-    flex: 1;
+
+    flex: 1 1 0;
+    height: 100%;
+
+    color: var(--textarea-primary-text-color);
     background-color: transparent;
-    color: var(
-      --tap-textarea-input-color,
-      var(--tap-sys-color-content-primary)
-    );
-    line-height: var(
-      --tap-textarea-input-line-height,
-      var(--tap-sys-typography-body-md-height)
-    );
-    font-size: var(
-      --tap-textarea-input-font-size,
-      var(--tap-sys-typography-body-md-size)
-    );
-    font-weight: var(
-      --tap-textarea-input-font-weight,
-      var(--tap-sys-typography-body-md-weight)
-    );
+
+    line-height: var(--tap-sys-typography-body-md-height);
+    font-size: var(--tap-sys-typography-body-md-size);
+    font-weight: var(--tap-sys-typography-body-md-weight);
     font-family: inherit;
+
     resize: none;
   }
 
   .input::placeholder {
-    color: var(
-      --tap-textarea-input-placeholder-color,
-      var(--tap-sys-color-content-tertiary)
-    );
-    line-height: var(
-      --tap-textarea-input-placeholder-line-height,
-      var(--tap-sys-typography-body-md-height)
-    );
-    font-size: var(
-      --tap-textarea-input-placeholder-font-size,
-      var(--tap-sys-typography-body-md-size)
-    );
-    font-weight: var(
-      --tap-textarea-input-placeholder-font-weight,
-      var(--tap-sys-typography-body-md-weight)
-    );
-    font-family: inherit;
-  }
+    color: var(--textarea-secondary-text-color);
 
-  :host([disabled]) .input,
-  :host([disabled]) .input::placeholder {
-    color: var(
-      --tap-input-disabled-container-color,
-      var(--tap-sys-color-content-disabled)
-    );
+    line-height: var(--tap-sys-typography-body-md-height);
+    font-size: var(--tap-sys-typography-body-md-size);
+    font-weight: var(--tap-sys-typography-body-md-weight);
+    font-family: inherit;
   }
 `;
--- packages/web-components/src/textarea/textarea.ts
+++ packages/web-components/src/textarea/textarea.ts
@@ -1,25 +1,39 @@
 import { type TemplateResult, html } from "lit";
+import { property } from "lit/decorators.js";
+import { classMap } from "lit/directives/class-map.js";
 import { ifDefined } from "lit/directives/if-defined.js";
 import { live } from "lit/directives/live.js";
 import { Input } from "../input";
 import { redispatchEvent } from "../utils";
 
 export class Textarea extends Input {
+  @property({ type: Number, attribute: "maxlength" })
+  public maxLength?: number;
+
+  @property({ type: Number, attribute: "minlength" })
+  public minLength?: number;
+
   private _handleChange(event: Event) {
     redispatchEvent(this, event);
   }
 
   protected renderInput(): TemplateResult {
+    const inputClasses = classMap({
+      input: true,
+      disabled: this.disabled,
+    });
+
     return html`
       <textarea
-        class="input"
+        class="${inputClasses}"
         id="textarea"
         .value=${live(this.value)}
         placeholder=${ifDefined(this.placeholder)}
+        ?required=${!!this.required}
         ?disabled=${this.disabled}
+        aria-describedby=${this.captionId}
         aria-label=${ifDefined(this.label)}
         aria-invalid=${this.error}
-        aria-disabled=${this.disabled ? "true" : "false"}
         @input=${this.handleInput}
         @change=${this._handleChange}
       >
