36689c45a7c9ad939f8ea0276656b19a59de46ad
mimshins <mostafa.sh.coderino@gmail.com>
2024-11-09 17:28:23 +0330

    refine badge component

--- packages/web-components/src/badge/badge.style.ts
+++ packages/web-components/src/badge/badge.style.ts
@@ -1,148 +1,95 @@
 import { css } from "lit";
 
 export default css`
-  .wrapper {
-    position: relative;
-    display: inline-flex;
+  *,
+  *::before,
+  *::after {
+    box-sizing: border-box;
   }
 
-  .badge {
+  .root:not(.dot).medium {
+    height: 1.5rem;
+    min-width: 1.5rem;
+  }
+
+  .root:not(.dot).small {
+    height: 1.25rem;
+    min-width: 1.25rem;
+  }
+
+  .root.dot {
+    width: 0.375rem;
+    height: 0.375rem;
+
+    padding: 0;
+  }
+
+  .root.success.high {
+    --badge-bg-color: var(--tap-sys-color-surface-positive);
+    --badge-color: var(--tap-sys-color-content-on-negative);
+  }
+
+  .root.info.high {
+    --badge-bg-color: var(--tap-sys-color-surface-accent);
+    --badge-color: var(--tap-sys-color-content-on-negative);
+  }
+
+  .root.error.high {
+    --badge-bg-color: var(--tap-sys-color-surface-negative);
+    --badge-color: var(--tap-sys-color-content-on-negative);
+  }
+
+  .root.warning.high {
+    --badge-bg-color: var(--tap-sys-color-surface-warning);
+    --badge-color: var(--tap-sys-color-content-on-warning);
+  }
+
+  .root.success.low {
+    --badge-bg-color: var(--tap-sys-color-surface-positive-light);
+    --badge-color: var(--tap-sys-color-content-positive);
+  }
+
+  .root.info.low {
+    --badge-bg-color: var(--tap-sys-color-surface-accent-light);
+    --badge-color: var(--tap-sys-color-content-accent);
+  }
+
+  .root.error.low {
+    --badge-bg-color: var(--tap-sys-color-surface-negative-light);
+    --badge-color: var(--tap-sys-color-content-negative);
+  }
+
+  .root.warning.low {
+    --badge-bg-color: var(--tap-sys-color-surface-warning-light);
+    --badge-color: var(--tap-sys-color-content-warning);
+  }
+
+  .root.neutral {
+    --badge-bg-color: var(--tap-sys-color-surface-disabled);
+    --badge-color: var(--tap-sys-color-content-tertiary);
+  }
+
+  .root {
     display: inline-flex;
     align-items: center;
+    justify-content: center;
+
     gap: var(--tap-sys-spacing-2);
-    font: inherit;
-    font-family: var(--tap-font-family, var(--tap-sys-font-family));
-    line-height: var(
-      --tap-badge-line-height,
-      var(--tap-sys-typography-body-xs-height)
-    );
-    font-size: var(--tap-badge-font-size, --tap-sys-typography-body-xs-size);
-    font-weight: var(
-      --tap-badge-font-weight,
-      var(--tap-sys-typography-body-xs-weight)
-    );
-    color: var(--tap-badge-color, --tap-sys-color-content-primary);
-    border-radius: var(--tap-badge-border-radius, var(--tap-sys-radius-4));
+
+    font-family: var(--tap-sys-font-family);
+    line-height: var(--tap-sys-typography-body-xs-height);
+    font-size: var(--tap-sys-typography-body-xs-size);
+    font-weight: var(--tap-sys-typography-body-xs-weight);
+
+    border-radius: var(--tap-sys-radius-full);
+
+    color: var(--badge-color);
+    background-color: var(--badge-bg-color);
+
+    padding-right: var(--tap-sys-spacing-3-1);
+    padding-left: var(--tap-sys-spacing-3-1);
   }
 
   .icon {
-    margin: var(--tap-badge-icon-margin, var(--tap-sys-spacing-2));
-  }
-
-  :host([type="pill"]) .badge,
-  :host([type="numeral"]) .badge {
-    border-radius: var(
-      --tap-badge-pill-numeral-border-radius,
-      var(--tap-sys-radius-4)
-    );
-    padding: var(
-        --tap-badge-pill-numeral-vertical-padding,
-        var(--tap-sys-spacing-2)
-      )
-      var(--tap-badge-pill-numeral-horizontal-padding, var(--tap-sys-spacing-4));
-  }
-
-  :host([type="dot"]) .badge {
-    /* FIXME: we dont have sizing tokens yet  */
-    width: var(--tap-badge-dot-width, 6px);
-    height: var(--tap-badge-dot-height, 6px);
-    /* FIXME: use spacing tokens  */
-    margin: var(--tap-badge-dot-margin, 3px);
-  }
-
-  :host([variant="info"]) .badge {
-    background-color: var(
-      --tap-badge-info-background-color,
-      var(--tap-sys-color-surface-accent)
-    );
-    color: var(
-      --tap-badge-info-color,
-      var(--tap-sys-color-content-on-negative)
-    );
-  }
-
-  :host([variant="success"]) .badge {
-    background-color: var(
-      --tap-badge-success-background-color,
-      var(--tap-sys-color-surface-positive)
-    );
-    color: var(
-      --tap-badge-success-color,
-      var(--tap-sys-color-content-on-negative)
-    );
-  }
-
-  :host([variant="error"]) .badge {
-    background-color: var(
-      --tap-badge-error-background-color,
-      var(--tap-sys-color-surface-negative)
-    );
-    color: var(
-      --tap-badge-error-color,
-      var(--tap-sys-color-content-on-negative)
-    );
-  }
-
-  :host([variant="warning"]) .badge {
-    background-color: var(
-      --tap-badge-warning-background-color,
-      var(--tap-sys-color-surface-warning)
-    );
-    color: var(
-      --tap-badge-warning-color,
-      var(--tap-sys-color-content-on-warning)
-    );
-  }
-
-  :host([variant="inverse"]) .badge {
-    background-color: var(
-      --tap-badge-inverse-background-color,
-      var(--tap-sys-color-surface-disabled)
-    );
-    color: var(
-      --tap-badge-inverse-color,
-      var(--tap-sys-color-content-tertiary)
-    );
-  }
-
-  :host([variant="info"][priority="low"]) .badge {
-    background-color: var(
-      --tap-badge-info-low-background-color,
-      var(--tap-sys-color-surface-accent-light)
-    );
-    color: var(--tap-badge-info-low-color, var(--tap-sys-color-content-accent));
-  }
-
-  :host([variant="success"][priority="low"]) .badge {
-    background-color: var(
-      --tap-badge-success-low-background-color,
-      var(--tap-sys-color-surface-positive-light)
-    );
-    color: var(
-      --tap-badge-success-low-color,
-      var(--tap-sys-color-content-positive)
-    );
-  }
-
-  :host([variant="error"][priority="low"]) .badge {
-    background-color: var(
-      --tap-badge-error-low-background-color,
-      var(--tap-sys-color-surface-negative-light)
-    );
-    color: var(
-      --tap-badge-error-low-color,
-      var(--tap-sys-color-content-negative)
-    );
-  }
-
-  :host([variant="warning"][priority="low"]) .badge {
-    background-color: var(
-      --tap-badge-warning-low-background-color,
-      var(--tap-sys-color-surface-warning-light)
-    );
-    color: var(
-      --tap-badge-warning-low-color,
-      var(--tap-sys-color-content-warning)
-    );
   }
 `;
--- packages/web-components/src/badge/badge.ts
+++ packages/web-components/src/badge/badge.ts
@@ -1,63 +1,65 @@
 import { html, LitElement } from "lit";
+import type { DirectiveResult } from "lit/async-directive";
 import { property } from "lit/decorators.js";
-import {
-  type BadgePriority,
-  type BadgeType,
-  type BadgeValue,
-  type BadgeVariant,
-} from "./types";
+import { classMap, type ClassMapDirective } from "lit/directives/class-map.js";
+import { Slots } from "./constants";
+
+type ClassMap = DirectiveResult<typeof ClassMapDirective>;
 
 export class Badge extends LitElement {
-  @property({ reflect: true })
-  public value?: BadgeValue = "";
+  @property({ type: String })
+  public value: string | number = "";
 
-  @property({ reflect: true })
-  public type?: BadgeType = "pill";
+  @property({ type: String })
+  public variant: "pill" | "numeral" | "dot" = "pill";
 
-  @property({ reflect: true })
-  public variant?: BadgeVariant = "inverse";
+  @property({ type: String })
+  public color: "success" | "error" | "info" | "warning" | "neutral" =
+    "neutral";
 
-  @property({ reflect: true })
-  public priority?: BadgePriority = "high";
+  @property({ type: String })
+  public priority: "high" | "low" = "high";
 
-  @property({ type: Boolean })
-  public leadingIcon? = false;
+  @property({ type: String })
+  public size: "medium" | "small" = "medium";
 
-  private _renderIcon() {
-    return html`
-      <svg
-        class="icon"
-        width="10"
-        height="10"
-        viewBox="0 0 10 10"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <path
-          fill-rule="evenodd"
-          clip-rule="evenodd"
-          d="M5 0C7.76 0 10 2.24 10 5C10 7.76 7.76 10 5 10C2.24 10 0 7.76 0 5C0 2.24 2.24 0 5 0ZM4.5 7.5H5.5V4.5H4.5V7.5ZM4.5 3.5H5.5V2.5H4.5V3.5Z"
-          fill="currentColor"
-        />
-      </svg>
-    `;
+  private _renderDotBadge(rootClasses: ClassMap) {
+    return html`<div class="${rootClasses}"></div>`;
   }
 
-  private _renderDotBadge() {
-    return html`<span class="badge"></span>`;
+  private _renderNumeralBadge(rootClasses: ClassMap) {
+    return html`<div class="${rootClasses}">${this.value ?? 0}</div>`;
   }
 
-  private _renderNormalBadge() {
+  private _renderPillBadge(rootClasses: ClassMap) {
     return html`
-      <span class="badge">
-        ${this.leadingIcon ? this._renderIcon() : null} ${this.value}
-      </span>
+      <div class="${rootClasses}">
+        <div
+          class=${Slots.ICON}
+          part=${Slots.ICON}
+        >
+          <slot name=${Slots.ICON}></slot>
+        </div>
+        ${this.value ?? ""}
+      </div>
     `;
   }
 
   protected override render() {
-    if (this.type === "dot") return this._renderDotBadge();
+    const rootClasses = classMap({
+      root: true,
+      [this.variant]: true,
+      [this.color]: true,
+      [this.priority]: true,
+      [this.size]: true,
+    });
 
-    return this._renderNormalBadge();
+    if (this.variant === "dot") return this._renderDotBadge(rootClasses);
+
+    if (this.variant === "numeral") {
+      return this._renderNumeralBadge(rootClasses);
+    }
+
+    return this._renderPillBadge(rootClasses);
   }
 }
