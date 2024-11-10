f5bd0aae61da05938e0522666f9b889f73c95f10
mimshins <mostafa.sh.coderino@gmail.com>
2024-11-10 11:48:16 +0330

    update badge-wrapper to position better

--- packages/web-components/src/badge-wrapper/badge-wrapper.style.ts
+++ packages/web-components/src/badge-wrapper/badge-wrapper.style.ts
@@ -1,16 +1,70 @@
 import { css } from "lit";
 
 export default css`
-  .wrapper {
+  .root.left .badge {
+    transform: translate(-50%, -50%);
+  }
+
+  .root.right .badge {
+    transform: translate(50%, -50%);
+  }
+
+  .root.rectangular.top .badge {
+    top: 0;
+  }
+
+  .root.rectangular.middle .badge {
+    top: 50%;
+  }
+
+  .root.rectangular.left .badge {
+    left: 0;
+  }
+
+  .root.rectangular.right .badge {
+    right: 0;
+  }
+
+  .root.circular.top .badge {
+    top: 15px;
+  }
+
+  .root.circular.middle .badge {
+    top: 50%;
+  }
+
+  .root.circular:not(.middle).left .badge {
+    left: 15px;
+  }
+
+  .root.circular:not(.middle).right .badge {
+    right: 15px;
+  }
+
+  .root.circular.middle.left .badge {
+    left: 0;
+  }
+
+  .root.circular.middle.right .badge {
+    right: 0;
+  }
+
+  .root {
     position: relative;
+
     display: inline-flex;
+    flex-shrink: 0;
+
+    vertical-align: middle;
+
+    z-index: 2;
   }
 
   .badge {
     display: inline-flex;
+    align-items: center;
+    justify-content: center;
+
     position: absolute;
-    top: 0;
-    left: 0;
-    transform: translate(-50%, -30%);
   }
 `;
--- packages/web-components/src/badge-wrapper/badge-wrapper.ts
+++ packages/web-components/src/badge-wrapper/badge-wrapper.ts
@@ -1,19 +1,39 @@
 import { html, LitElement } from "lit";
+import { property } from "lit/decorators.js";
+import { classMap } from "lit/directives/class-map.js";
+import { Slots } from "./constants";
 
 export class BadgeWrapper extends LitElement {
+  @property({ type: String, attribute: "anchor-shape" })
+  public anchorShape: "rectangular" | "circular" = "rectangular";
+
+  @property({ type: String, attribute: "badge-side" })
+  public badgeSide: "left" | "right" = "right";
+
+  @property({ type: String, attribute: "badge-alignment" })
+  public badgeAlignment: "top" | "middle" = "top";
+
   protected override render() {
+    const rootClasses = classMap({
+      root: true,
+      [this.anchorShape]: true,
+      [this.badgeAlignment]: true,
+      [this.badgeSide]: true,
+    });
+
     return html`
-      <span
-        class="wrapper"
-        part="wrapper"
+      <div
+        class=${rootClasses}
+        part="root"
       >
         <slot></slot>
-        <slot
-          class="badge"
-          name="badge"
-          part="badge"
-        ></slot>
-      </span>
+        <div
+          class=${Slots.BADGE}
+          part=${Slots.BADGE}
+        >
+          <slot name=${Slots.BADGE}></slot>
+        </div>
+      </div>
     `;
   }
 }
