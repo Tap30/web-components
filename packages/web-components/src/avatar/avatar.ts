41588374b3d31c97c70de88bb7b9734dc554601e
Mostafa Shamsitabar <50550858+mimshins@users.noreply.github.com>
2024-11-17 16:35:22 +0330

    refactor(avatar): update to match conventions (#260)

--- packages/web-components/src/avatar/avatar.ts
+++ packages/web-components/src/avatar/avatar.ts
@@ -1,21 +1,40 @@
 import { LitElement, type PropertyValues, html } from "lit";
 import { property, state } from "lit/decorators.js";
+import { classMap } from "lit/directives/class-map.js";
 
 export class Avatar extends LitElement {
   @state()
   private _hasError = false;
 
+  /**
+   * The image source to use for the avatar.
+   */
   @property()
   public image = "";
 
+  /**
+   * A label to use to describe the avatar to assistive devices.
+   */
   @property()
   public label = "";
 
+  /**
+   * The alternative text description of the avatar image, used for accessibility.
+   */
+  @property()
+  public alt = "";
+
+  /**
+   * Indicates how the browser should load the image.
+   */
   @property()
   public loading: "eager" | "lazy" = "eager";
 
+  /**
+   * The size of the avatar.
+   */
   @property()
-  public size: "xSmall" | "small" | "medium" | "large" | "xLarge" = "medium";
+  public size: "xs" | "sm" | "md" | "lg" | "xlg" | "xxlg" = "md";
 
   protected override updated(changed: PropertyValues) {
     if (changed.has("image")) {
@@ -26,8 +45,9 @@ export class Avatar extends LitElement {
   private _renderImage() {
     return html`
       <img
-        alt=""
         part="image"
+        class="image"
+        alt=${this.alt}
         src="${this.image}"
         loading="${this.loading}"
         @error="${() => (this._hasError = true)}"
@@ -38,20 +58,25 @@ export class Avatar extends LitElement {
   private _renderPlaceholder() {
     return html` <div
       part="placeholder"
-      aria-hidden="true"
       class="placeholder"
+      aria-hidden="true"
     >
       <slot></slot>
     </div>`;
   }
 
   protected override render() {
+    const rootClasses = classMap({
+      root: true,
+      [this.size]: true,
+    });
+
     return html`
       <div
         role="img"
         aria-label=${this.label}
-        part="avatar"
-        class="avatar"
+        part="root"
+        class=${rootClasses}
       >
         ${this.image && !this._hasError
           ? this._renderImage()
