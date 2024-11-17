01385895e16072086f82c331b0e01f56c7f96395
mimshins <mostafa.sh.coderino@gmail.com>
2024-11-17 16:32:56 +0330

    refactor avatar

--- packages/web-components/src/avatar/avatar.style.ts
+++ packages/web-components/src/avatar/avatar.style.ts
@@ -2,69 +2,67 @@ import { css } from "lit";
 
 export default css`
   :host {
-    box-sizing: border-box;
+    display: inline-block;
   }
 
-  :host *,
-  :host *::before,
-  :host *::after {
-    box-sizing: inherit;
+  *,
+  *::before,
+  *::after {
+    box-sizing: border-box;
   }
 
   [hidden] {
     display: none !important;
   }
 
-  .avatar {
-    background-color: var(
-      --tap-avatar-background-color,
-      var(--tap-sys-color-surface-secondary)
-    );
-    border: 1px solid
-      var(--tap-avatar-border-color, var(--tap-sys-color-border-primary));
-    border-radius: var(--tap-avatar-border-radius, var(--tap-sys-radius-full));
+  .root {
+    background-color: var(--tap-sys-color-surface-secondary);
+    box-shadow: 0 0 0 1px var(--tap-sys-color-border-primary);
+    border-radius: 50%;
+
     display: inline-flex;
     align-items: center;
     justify-content: center;
+
+    vertical-align: middle;
   }
 
-  .avatar img,
-  .avatar .placeholder,
-  .avatar ::slotted(svg) {
+  .image,
+  .placeholder,
+  .root ::slotted(svg) {
     width: 100%;
     height: 100%;
-    border-radius: inherit;
+
+    border-radius: 50%;
   }
 
-  :host([size="xxSmall"]) .avatar {
-    width: var(--tap-avatar-width-xxSmall, var(--tap-sys-spacing-8));
-    height: var(--tap-avatar-height-, vxxSmallar(--tap-sys-spacing-8));
+  .root.xs {
+    width: 1.5rem;
+    height: 1.5rem;
   }
 
-  :host([size="xSmall"]) .avatar {
-    width: var(--tap-avatar-width-xSmall, var(--tap-sys-spacing-9));
-    height: var(--tap-avatar-height-xSmall, var(--tap-sys-spacing-9));
+  .root.sm {
+    width: 2rem;
+    height: 2rem;
   }
 
-  :host([size="small"]) .avatar {
-    width: var(--tap-avatar-width-small, var(--tap-sys-spacing-10));
-    height: var(--tap-avatar-height-small, var(--tap-sys-spacing-10));
+  .root.md {
+    width: 2.5rem;
+    height: 2.5rem;
   }
 
-  :host([size="medium"]) .avatar {
-    width: var(--tap-avatar-width-medium, var(--tap-sys-spacing-11));
-    height: var(--tap-avatar-height-medium, var(--tap-sys-spacing-11));
+  .root.lg {
+    width: 3rem;
+    height: 3rem;
   }
 
-  // TODO: add to tokens
-  :host([size="large"]) .avatar {
-    width: var(--tap-avatar-width-large, 56px);
-    height: var(--tap-avatar-height-large, 56px);
+  .root.xlg {
+    width: 3.5rem;
+    height: 3.5rem;
   }
 
-  // TODO: add to tokens
-  :host([size="xLarge"]) .avatar {
-    width: var(--tap-avatar-width-xLarge, 72px);
-    height: var(--tap-avatar-height-xLarge, 72px);
+  .root.xxlg {
+    width: 4.5rem;
+    height: 4.5rem;
   }
 `;
--- packages/web-components/src/avatar/index.ts
+++ packages/web-components/src/avatar/index.ts
@@ -1,6 +1,8 @@
 import { customElement } from "lit/decorators.js";
-import { Avatar } from "./avatar.js";
-import styles from "./avatar.style.js";
+import { Avatar } from "./avatar";
+import styles from "./avatar.style";
+
+export { Slots } from "./constants";
 
 /**
  * @summary Display user profile image, initials or fallback icon
@@ -8,35 +10,10 @@ import styles from "./avatar.style.js";
  * @slot - The default slot to use when image is not present.
  *
  * @prop {string} [label=''] - A label to use to describe the avatar to assistive devices.
+ * @prop {string} [alt=''] - The alternative text description of the avatar image, used for accessibility.
  * @prop {string} [image=''] - The image source to use for the avatar.
- * @prop {'eager' | 'lazy'} [loading='eager'] -  Indicates how the browser should load the image.
+ * @prop {'eager' | 'lazy'} [loading='eager'] - Indicates how the browser should load the image.
  * @prop {'xSmall' | 'small' | 'medium' | 'large' | 'xLarge'} [size='medium'] - The size of the avatar.
- *
- * @csspart [avatar] - The container that wraps the avatar component.
- * @csspart [placeholder] - The container that wraps the avatar's placeholder.
- * @csspart [image] - The avatar image. Only shown when the image is present.
- *
- * @cssprop [--tap-avatar-background-color=--tap-sys-color-surface-secondary]
- * @cssprop [--tap-avatar-border-color=--tap-sys-color-border-primary]
- * @cssprop [--tap-avatar-border-radius=--tap-sys-radius-full]
- *
- * @cssprop [--tap-avatar-width-xxSmall=--tap-sys-spacing-8]
- * @cssprop [--tap-avatar-height-xxSmall=--tap-sys-spacing-8]
- *
- * @cssprop [--tap-avatar-width-xSmall=--tap-sys-spacing-9]
- * @cssprop [--tap-avatar-height-xSmall=--tap-sys-spacing-9]
- *
- * @cssprop [--tap-avatar-width-small=--tap-sys-spacing-10]
- * @cssprop [--tap-avatar-height-small=--tap-sys-spacing-10]
- *
- * @cssprop [--tap-avatar-width-medium=--tap-sys-spacing-11]
- * @cssprop [--tap-avatar-height-medium=--tap-sys-spacing-11]
- *
- * @cssprop [--tap-avatar-width-large=56px]
- * @cssprop [--tap-avatar-height-large=56px]
- *
- * @cssprop [--tap-avatar-width-xLarge=72px]
- * @cssprop [--tap-avatar-height-xLarge=72px]
  */
 @customElement("tap-avatar")
 export class TapAvatar extends Avatar {
