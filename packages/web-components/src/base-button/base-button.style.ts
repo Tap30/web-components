c1beb41519ea4e6b758a8d5fb2f6f60281f0b393
Amir Hossein Alibakhshi <amir78729@gmail.com>
2024-11-19 20:35:11 +0330

    refactor(button): change styles structures

--- packages/web-components/src/base-button/base-button.style.ts
+++ packages/web-components/src/base-button/base-button.style.ts
@@ -23,9 +23,82 @@ export default css`
     place-content: center;
     place-items: center;
     position: relative;
+    -webkit-tap-highlight-color: transparent;
   }
 
-  .button {
+  :host([disabled]) {
+    cursor: not-allowed;
+    pointer-events: none;
+  }
+
+  .root.sm .content {
+    --button-line-height: var(--tap-sys-typography-label-sm-height);
+    --button-font-size: var(--tap-sys-typography-label-sm-size);
+    --button-font-weight: var(--tap-sys-typography-label-sm-weight);
+  }
+
+  .root.md .content {
+    --button-line-height: var(--tap-sys-typography-label-sm-height);
+    --button-font-size: var(--tap-sys-typography-label-sm-size);
+    --button-font-weight: var(--tap-sys-typography-label-sm-weight);
+  }
+
+  .root.lg .content {
+    --button-line-height: var(--tap-sys-typography-label-lg-height);
+    --button-font-size: var(--tap-sys-typography-label-lg-size);
+    --button-font-weight: var(--tap-sys-typography-label-lg-weight);
+  }
+
+  .root.primary {
+    --button-background: var(--tap-sys-color-surface-inverse-primary);
+    --button-color: var(--tap-sys-color-content-on-inverse);
+  }
+
+  .root.ghost {
+    --button-background: var(--tap-sys-color-surface-tertiary);
+    --button-color: var(--tap-sys-color-content-primary);
+  }
+
+  .root.naked {
+    --button-background: transparent;
+    --button-color: var(--tap-sys-color-content-primary);
+  }
+
+  .root.elevated {
+    --button-background: var(--tap-sys-color-surface-primary);
+    --button-color: var(--tap-sys-color-content-primary);
+    box-shadow: 0 4px 16px 0 #0000001a;
+  }
+
+  .root.destructive {
+    --button-background: var(--tap-sys-color-surface-negative-light);
+    --button-color: var(--tap-sys-color-content-negative);
+  }
+
+  .root.brand {
+    --button-background: var(--tap-sys-color-gradient-brand);
+    --button-color: var(--tap-sys-color-content-on-inverse);
+  }
+
+  .root:not(.naked):disabled {
+    --button-background: var(--tap-sys-color-surface-disabled);
+    --button-color: var(--tap-sys-color-content-disabled);
+  }
+
+  .root.naked:disabled {
+    --button-background: transparent;
+    --button-color: var(--tap-sys-color-content-disabled);
+  }
+
+  .root:not(.primary):active .overlay {
+    --button-overlay-color: var(--tap-sys-color-surface-overlay-light);
+  }
+
+  .root.primary:active .overlay {
+    --button-overlay-color: var(--tap-sys-color-surface-inverse-secondary);
+  }
+
+  .root {
     position: relative;
     cursor: inherit;
     display: inline-flex;
@@ -39,151 +112,24 @@ export default css`
     font: inherit;
     gap: inherit;
     width: 100%;
+    background: var(--button-background);
+    color: var(--button-color);
     font-family: var(--tap-font-family, var(--tap-sys-font-family));
-    border-radius: var(--tap-button-radius, var(--tap-sys-radius-full));
+    border-radius: var(--tap-sys-radius-full);
   }
 
-  .cover {
-    display: none;
+  .overlay {
+    opacity: 0;
     position: absolute;
     inset: 0;
     border-radius: inherit;
-    background-color: var(
-      --tap-button-color-surface-cover,
-      var(--tap-sys-color-surface-overlay-light)
-    );
+    background-color: var(--button-overlay-color);
+    z-index: 1;
   }
 
-  .button:active .cover {
-    display: inline;
-  }
-
-  :host([disabled]) {
-    pointer-events: none;
-    cursor: default;
-  }
-
-  :host([disabled]) .cover {
-    display: inline;
-    /* FIXME: we need to define a overlay color for disabled since color-mix is not well supported */
-    background-color: color-mix(
-      in srgb,
-      var(
-        --tap-button-color-surface-disabled,
-        var(--tap-sys-color-surface-disabled)
-      ),
-      transparent 50%
-    );
-  }
-
-  :host([size="small"]) .button {
-    line-height: var(
-      --tap-button-typography-label-sm-height,
-      var(--tap-sys-typography-label-sm-height)
-    );
-    font-size: var(
-      --tap-button-typography-label-sm-size,
-      var(--tap-sys-typography-label-sm-size)
-    );
-    font-weight: var(
-      --tap-button-typography-label-sm-weight,
-      var(--tap-sys-typography-label-sm-weight)
-    );
-  }
-
-  :host([size="medium"]) .button {
-    line-height: var(
-      --tap-button-typography-label-md-height,
-      var(--tap-sys-typography-label-sm-height)
-    );
-    font-size: var(
-      --tap-button-typography-label-md-size,
-      var(--tap-sys-typography-label-sm-size)
-    );
-    font-weight: var(
-      --tap-button-typography-label-md-weight,
-      var(--tap-sys-typography-label-sm-weight)
-    );
-  }
-
-  :host([size="large"]) .button {
-    line-height: var(
-      --tap-button-typography-label-lg-height,
-      var(--tap-sys-typography-label-lg-height)
-    );
-    font-size: var(
-      --tap-button-typography-label-lg-size,
-      var(--tap-sys-typography-label-lg-size)
-    );
-    font-weight: var(
-      --tap-button-typography-label-lg-weight,
-      var(--tap-sys-typography-label-lg-weight)
-    );
-  }
-
-  :host([variant="primary"]) .button {
-    background-color: var(
-      --tap-button-color-surface-inverse-primary,
-      var(--tap-sys-color-surface-inverse-primary)
-    );
-    color: var(
-      --tap-button-color-content-on-inverse,
-      var(--tap-sys-color-content-on-inverse)
-    );
-  }
-
-  :host([variant="ghost"]) .button {
-    background-color: var(
-      --tap-button-color-surface-tertiary,
-      var(--tap-sys-color-surface-tertiary)
-    );
-    color: var(
-      --tap-button-color-content-primary,
-      var(--tap-sys-color-content-primary)
-    );
-  }
-
-  :host([variant="naked"]) .button {
-    background-color: transparent;
-    color: var(
-      --tap-button-color-content-primary,
-      var(--tap-sys-color-content-primary)
-    );
-  }
-
-  :host([variant="elevated"]) .button {
-    background-color: var(
-      --tap-button-color-surface-primary,
-      var(--tap-sys-color-surface-primary)
-    );
-    color: var(
-      --tap-button-color-content-primary,
-      var(--tap-sys-color-content-primary)
-    );
-    /* FIXME: we dont have shadow tokens yet  */
-    box-shadow: 0 4px 16px 0 #0000001a;
-  }
-
-  :host([variant="destructive"]) .button {
-    background-color: var(
-      --tap-button-color-surface-destructive,
-      var(--tap-sys-color-surface-negative-light)
-    );
-    color: var(
-      --tap-button-color-content-destructive,
-      var(--tap-sys-color-content-negative)
-    );
-  }
-
-  :host([variant="brand"]) .button {
-    background: var(
-      --tap-button-color-gradient-brand,
-      var(--tap-sys-color-gradient-brand)
-    );
-    color: var(
-      --tap-button-color-content-on-inverse,
-      var(--tap-sys-color-content-on-inverse)
-    );
+  .root:active .overlay,
+  .root:disabled .overlay {
+    opacity: 1;
   }
 
   .spinner {
@@ -192,9 +138,20 @@ export default css`
     align-items: center;
     left: 50%;
     transform: translateX(-50%);
+    z-index: 2;
   }
 
   .content {
+    line-height: var(--button-line-height);
+    font-size: var(--button-font-size);
+    font-weight: var(--button-font-weight);
+    z-index: 2;
+  }
+
+  .root.loading .spinner {
+    visibility: visible;
+  }
+  .root.loading .content {
     visibility: hidden;
   }
 `;
