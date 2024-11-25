f7bcd72298d575febdc2cbe508119a940127acf6
mimshins <mostafa.sh.coderino@gmail.com>
2024-11-25 13:13:34 +0330

    update input styles

--- packages/web-components/src/input/input.style.ts
+++ packages/web-components/src/input/input.style.ts
@@ -13,22 +13,26 @@ export default css`
 
   .field.error {
     --input-caption-color: var(--tap-sys-color-content-negative);
-    --input-container-bg-color: var(--tap-sys-color-surface-negative-light);
-    --input-container-border-color: var(--tap-sys-color-border-negative);
+    --input-control-bg-color: var(--tap-sys-color-surface-negative-light);
+    --input-control-border-color: var(--tap-sys-color-border-negative);
   }
 
   .field.disabled {
     --input-label-color: var(--tap-sys-color-content-disabled);
     --input-caption-color: var(--tap-sys-color-content-disabled);
-    --input-container-bg-color: var(--tap-sys-color-surface-disabled);
-    --input-container-border-color: transparent;
+    --input-control-bg-color: var(--tap-sys-color-surface-disabled);
+    --input-control-border-color: transparent;
+  }
+
+  .field.readonly {
+    --input-control-bg-color: var(--tap-sys-color-surface-disabled);
   }
 
   .field {
     --input-label-color: var(--tap-sys-color-content-primary);
     --input-caption-color: var(--tap-sys-color-content-tertiary);
-    --input-container-bg-color: var(--tap-sys-color-surface-tertiary);
-    --input-container-border-color: transparent;
+    --input-control-bg-color: var(--tap-sys-color-surface-tertiary);
+    --input-control-border-color: transparent;
 
     direction: rtl;
     font-family: var(--tap-sys-font-family);
@@ -55,7 +59,7 @@ export default css`
     font-weight: var(--tap-sys-typography-body-sm-weight);
   }
 
-  .container {
+  .control {
     height: 3.25rem;
     padding: 0 var(--tap-sys-spacing-6);
 
@@ -65,17 +69,17 @@ export default css`
 
     gap: var(--tap-sys-spacing-4);
 
-    background-color: var(--input-container-bg-color);
+    background-color: var(--input-control-bg-color);
     border-radius: var(--tap-sys-radius-3);
-    border: var(--tap-sys-stroke-2) solid var(--input-container-border-color);
+    border: var(--tap-sys-stroke-2) solid var(--input-control-border-color);
   }
 
-  .container input,
-  .container textarea {
+  .control input,
+  .control textarea {
     caret-color: var(--tap-sys-color-surface-accent);
   }
 
-  .container:focus-within {
+  .field:not(.no-control-focus-border) .control:focus-within {
     background-color: var(--tap-sys-color-surface-secondary);
     border: var(--tap-sys-stroke-2) solid
       var(--tap-sys-color-border-inverse-primary);
