1fb806dab917a3471d46a31e6288976460e18c81
mimshins <mostafa.sh.coderino@gmail.com>
2024-11-27 15:53:47 +0330

    add focus-ring internal tokens

--- packages/web-components/src/internals/tokens.ts
+++ packages/web-components/src/internals/tokens.ts
@@ -1,3 +1,5 @@
+import { unsafeCSS } from "lit";
+
 export const Z_INDEXES = {
   0: 1000,
   1: 1010,
@@ -6,3 +8,8 @@ export const Z_INDEXES = {
   4: 1040,
   5: 1050,
 };
+
+export const FOCUS_RING_LINE = unsafeCSS(
+  "var(--tap-sys-stroke-2) solid var(--tap-sys-color-content-accent)",
+);
+export const FOCUS_RING_OFFSET = unsafeCSS("var(--tap-sys-stroke-2)");
