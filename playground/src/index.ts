50520113c509a350455eff79c975aed31b384795
Amir Hossein Alibakhshi <amir78729@gmail.com>
2024-11-30 00:34:08 +0330

    fix(playground): reset

--- playground/src/index.ts
+++ playground/src/index.ts
@@ -1,6 +1,3 @@
 import "@tapsioss/theme";
 // Import modules using path aliases
 // i.e. `import "@tapsioss/web-components/avatar";`
-
-import "@tapsioss/web-components/notice";
-import "@tapsioss/web-components/button";
