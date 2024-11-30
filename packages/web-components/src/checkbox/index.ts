ab4494989876bf2ce8a940f4a77daf24910e3d1f
mimshins <mostafa.sh.coderino@gmail.com>
2024-11-30 10:16:07 +0330

    add api docs

--- packages/web-components/src/checkbox/index.ts
+++ packages/web-components/src/checkbox/index.ts
@@ -4,12 +4,23 @@ import { Checkbox } from "./checkbox";
 import styles from "./checkbox.style";
 
 /**
- * @summary A checkbox component with support for checked and indeterminate states.
+ * @summary A checkbox component.
  *
  * @prop {boolean} [checked=false] - Indicates whether the checkbox is checked.
  * @prop {boolean} [indeterminate=false] - Indicates whether the checkbox is in an indeterminate state.
  * @prop {boolean} [disabled=false] - Indicates whether the checkbox is disabled.
  * @prop {string} [value='on'] - The value of the checkbox when it is checked.
+ * @prop {boolean} [error=false] - Gets or sets whether or not the input is in a visually invalid state.\
+ * This error state overrides the error state controlled by
+ * `reportValidity()`.
+ * @prop {string} label - The label of the input.\
+ * Displays a bound `label` element when `showLabel` is `true`.
+ * Otherwise, sets an `aria-label` attribute.
+ * @prop {boolean} [show-label=false] - Whether or not the label is visible.
+ * @prop {boolean} [required=false] - Indicates that the user must specify a value for the input before the
+ * owning form can be submitted and will render an error state when
+ * `reportValidity()` is invoked when value is empty.\
+ * https://developer.mozilla.org/en-US/docs/Web/HTML/Attributes/required
  */
 @customElement("tap-checkbox")
 export class TapCheckbox extends Checkbox {
