8348826a15b85d00f4279326445830b82cdaf263
mimshins <mostafa.sh.coderino@gmail.com>
2024-12-01 13:01:00 +0330

    update util

--- packages/web-components/src/utils/wait-a-microtask.ts
+++ packages/web-components/src/utils/wait-a-microtask.ts
@@ -4,10 +4,8 @@
  * Schedules a microtask but should be used to allow any other microtasks
  * (like other event handlers) to complete first.
  */
-const waitAMicrotask = () => {
-  return new Promise<void>(resolve => {
-    resolve();
-  });
+const waitAMicrotask = async () => {
+  return Promise.resolve<void>(void 0);
 };
 
 export default waitAMicrotask;
