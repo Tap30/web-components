051a99f89bc15e20650e3ffdb6f103cec74b2861
mimshins <mostafa.sh.coderino@gmail.com>
2024-12-01 11:02:59 +0330

    add analyzer annotations

--- packages/web-components/src/switch/index.ts
+++ packages/web-components/src/switch/index.ts
@@ -9,9 +9,10 @@ import styles from "./switch.style";
  * @prop {boolean} [selected=false] - Indicates whether the switch is selected.
  * @prop {boolean} [disabled=false] - Indicates whether the switch is disabled.
  * @prop {string} [value='on'] - The value of the switch when it is checked.
- * @prop {string} label - The label of the input.\
- * Displays a bound `label` element when `showLabel` is `true`.
- * Otherwise, sets an `aria-label` attribute.
+ * @prop {string} label - Defines a string value that can be used to name switch input.\
+ * https://developer.mozilla.org/en-US/docs/Web/Accessibility/ARIA/Attributes/aria-label
+ * @prop {string} labelledby - Identifies the element (or elements) that labels the switch input.\
+ * https://developer.mozilla.org/en-US/docs/Web/Accessibility/ARIA/Attributes/aria-labelledby
  * @prop {boolean} [required=false] - Indicates that the user must specify a value for the input before the
  * owning form can be submitted and will render an error state when
  * `reportValidity()` is invoked when value is empty.\
