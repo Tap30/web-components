e4853066729f8dc97653947a79a7eaf4c8498810
Mostafa Shamsitabar <50550858+mimshins@users.noreply.github.com>
2024-12-04 17:00:57 +0330

    refactor(stepper): rewrite stepper to follow best practices (#286)

--- packages/web-components/src/stepper/index.ts
+++ packages/web-components/src/stepper/index.ts
@@ -3,29 +3,47 @@ import { Stepper } from "./stepper.js";
 import styles from "./stepper.style.js";
 
 /**
- * @summary A form item that shows a value and can be increased or decreased by tw buttons
+ * @summary The stepper input component.
  *
- * @prop {string} [unit=''] - A single quantity of the value
- * @prop {boolean} [disabled=false] - If `true`, the component is disabled.
- * @prop {number} [step=1] - The number to which the current value is increased or decreased.
- * It can be an integer or decimal
- * @prop {'small' | 'medium'} [size='medium'] - The size of icons and font.
- * @prop {number} [min=-Infinity] - The min value.
- * @prop {number} [max=Infinity] - The max value.
- * @prop {number} [fullWidth=false] - If `true` the component will fill the parent.
- * @prop {number} [value=0] - The current value.
+ * @prop {"sm" | "md"} [size="md"] - The size of the input.
+ * @prop {string} [value=""] - The current value of the input. It is always a string.
+ * @prop {string} [name=""] - The HTML name to use in form submission.
+ * @prop {string} [unit=""] - The unit of the value.
+ * @prop {string} [valueText=""] -
+ * Defines the human-readable text alternative of value.
  *
- * @fires stepper-change - Fired when the value of the stepper changes.
+ * https://developer.mozilla.org/en-US/docs/Web/Accessibility/ARIA/Attributes/aria-valuetext
+ * @prop {string} [label=""] -
+ * Defines a string value that can be used to name input.
  *
- * @csspart [stepper] - The container that wraps the stepper component.
- * @csspart [decrease-button] - The button that contains minus icon and decrease the value.
- * @csspart [increase-button] - The button that contains plus icon and increases the value.
- * @csspart [value] - The `p` tag that shows the `value`.
- * @csspart [unit] - The `p` tag that shows the `unit` and will be hidden if `unit` not exists.
+ * https://developer.mozilla.org/en-US/docs/Web/Accessibility/ARIA/Attributes/aria-label
+ * @prop {string} [labelledby=""] -
+ * Identifies the element (or elements) that labels the input.
  *
- * @cssprop [--tap-font-family=--tap-sys-font-family]
- * @cssprop [--tap-stepper-typography-label-sm-size=--tap-sys-typography-label-md-size]
- * @cssprop [--tap-stepper-typography-label-md-size=--tap-sys-typography-label-lg-size]
+ * https://developer.mozilla.org/en-US/docs/Web/Accessibility/ARIA/Attributes/aria-labelledby
+ * @prop {boolean} [readonly=false] -
+ * Indicates whether or not a user should be able to edit the input's
+ * value.
+ *
+ * https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input#readonly
+ * @prop {string} [max="100"] -
+ * Defines the maximum value in the range of permitted values.
+ * Defaults to "100".
+ *
+ * https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input#max
+ * @prop {string} [min="0"] -
+ * Defines the minimum value in the range of permitted values.
+ * Defaults to "0".
+ *
+ * https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input#min
+ * @prop {string} [step="1"] -
+ * Returns or sets the element's step attribute, which works with min and max
+ * to limit the increments/decrements at which the input value can be set.
+ * Defaults to "1".
+ *
+ * https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input#step
+ *
+ * @fires {Event} change - Fires when value changes.
  */
 @customElement("tap-stepper")
 export class TapStepper extends Stepper {
@@ -34,6 +52,6 @@ export class TapStepper extends Stepper {
 
 declare global {
   interface HTMLElementTagNameMap {
-    "tap-stepper": Stepper;
+    "tap-stepper": TapStepper;
   }
 }
