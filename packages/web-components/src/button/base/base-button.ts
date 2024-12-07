38c39a745d8596111613033d518fc767a2dc6ba6
mimshins <mostafa.sh.coderino@gmail.com>
2024-12-07 18:15:57 +0330

    refactor(buttons): update focusability and styles

--- packages/web-components/src/button/base/base-button.ts
+++ packages/web-components/src/button/base/base-button.ts
@@ -1,17 +1,26 @@
-import { html, LitElement, nothing, type TemplateResult } from "lit";
+import "../../spinner";
+
+import {
+  html,
+  LitElement,
+  nothing,
+  type PropertyValues,
+  type TemplateResult,
+} from "lit";
 import { property } from "lit/decorators.js";
 import { classMap } from "lit/directives/class-map.js";
 import { ifDefined } from "lit/directives/if-defined.js";
-import "../../spinner";
 import {
   type FormSubmitter,
   type FormSubmitterType,
+  isFocusable,
   setupFormSubmitter,
   withElementInternals,
+  withFocusable,
 } from "../../utils";
 import { internals } from "../../utils/mixins/with-element-internals";
 
-const BaseClass = withElementInternals(LitElement);
+const BaseClass = withElementInternals(withFocusable(LitElement));
 
 export abstract class BaseButton extends BaseClass implements FormSubmitter {
   static {
@@ -38,13 +47,13 @@ export abstract class BaseButton extends BaseClass implements FormSubmitter {
    * The type of the button.
    */
   @property()
-  public type: FormSubmitterType = "submit";
+  public type: FormSubmitterType = "button";
 
   /**
    * The accessible label for the button.
    */
   @property()
-  public label?: string;
+  public label: string = "";
 
   /**
    * Whether the button is in a loading state.
@@ -73,12 +82,27 @@ export abstract class BaseButton extends BaseClass implements FormSubmitter {
     | "destructive"
     | "brand" = "primary";
 
-  constructor() {
-    super();
-  }
-
   public get form() {
     return this[internals].form;
+  }
+
+  private _updateFocusability() {
+    if (this.disabled) {
+      this.removeAttribute("tabindex");
+      this[isFocusable] = false;
+    } else this[isFocusable] = true;
+  }
+
+  public override connectedCallback() {
+    super.connectedCallback();
+
+    this._updateFocusability();
+  }
+
+  protected override updated(changed: PropertyValues<this>) {
+    super.updated(changed);
+
+    if (changed.has("disabled")) this._updateFocusability();
   }
 
   /**
@@ -140,12 +164,11 @@ export abstract class BaseButton extends BaseClass implements FormSubmitter {
         id="root"
         part="root"
         class=${rootClasses}
+        tabindex=${this.tabIndex}
         @click=${this._handleClick}
         ?disabled=${this.disabled}
-        type=${ifDefined(this.type)}
-        aria-label=${ifDefined(this.label)}
-        aria-labelledby=${nothing}
-        aria-describedby=${nothing}
+        type=${this.type}
+        aria-label=${this.label || nothing}
       >
         <span class="overlay"></span>
         ${this._renderBody()}
