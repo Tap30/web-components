32dd6ea8cd24cb4728049a7a2ae03b827b99a91b
mimshins <mostafa.sh.coderino@gmail.com>
2024-12-25 01:03:56 +0330

    fix(focus-trapper): resolve issue with stacked modals

--- packages/web-components/src/utils/controllers/FocusTrapper.ts
+++ packages/web-components/src/utils/controllers/FocusTrapper.ts
@@ -7,6 +7,7 @@ import {
 } from "lit";
 import { styleMap } from "lit/directives/style-map.js";
 import { getDeepActiveElement, isElementFocusable } from "../dom";
+import { runAfterRepaint } from "../event-loop-execution";
 
 type Host = ReactiveControllerHost & LitElement;
 
@@ -15,15 +16,18 @@ const TrapperTypes = {
   END: "end",
 } as const;
 
+const HOST_INSTANCE_DATA_ATTRIBUTE = "data-focus-trapper-host-instance";
+const HOST_ENABLED_DATA_ATTRIBUTE = "data-focus-trapper-enabled";
+
 class FocusTrapper implements ReactiveController {
   private readonly _host: Host;
 
-  private _isEnabled = true;
-  private _ignoreFocusChanges = false;
+  private _isEnabled = false;
 
   private readonly _resolveTreeRoot: () => HTMLElement | null;
 
   private _lastFocused: HTMLElement | null = null;
+  private _ignoreFocusChanges = false;
 
   constructor(host: Host, resolveTreeRoot: () => HTMLElement | null) {
     host.addController(this);
@@ -32,10 +36,14 @@ class FocusTrapper implements ReactiveController {
     this._resolveTreeRoot = resolveTreeRoot;
 
     this._handleDocumentFocus = this._handleDocumentFocus.bind(this);
+    this.sendFocus = this.sendFocus.bind(this);
   }
 
   public set enabled(isEnabled: boolean) {
+    if (this._isEnabled === isEnabled) return;
+
     this._isEnabled = isEnabled;
+    this._host.setAttribute(HOST_ENABLED_DATA_ATTRIBUTE, `${isEnabled}`);
   }
 
   public get enabled() {
@@ -47,7 +55,68 @@ class FocusTrapper implements ReactiveController {
 
     const [firstFocusableChild] = this._getFirstAndLastFocusableChildren();
 
-    firstFocusableChild?.focus();
+    this._tryFocus(firstFocusableChild);
+  }
+
+  // TODO: Add good caching mechanism for top most instance
+  private get _topMostInstance() {
+    const instances = Array.from(
+      document.querySelectorAll<Host>(`[${HOST_INSTANCE_DATA_ATTRIBUTE}]`),
+    );
+
+    const enabledInstances = instances.filter(instance => {
+      const isEnabledStr =
+        instance.getAttribute(HOST_ENABLED_DATA_ATTRIBUTE) ?? "false";
+
+      return isEnabledStr === "true";
+    });
+
+    if (enabledInstances.length === 0) return null;
+
+    const { topMost } = enabledInstances.reduce<{
+      topMost: Host | null;
+      topMostZIndex: number;
+    }>(
+      (result, instance) => {
+        const zIndex = getComputedStyle(instance).zIndex;
+
+        if (zIndex === "auto") {
+          if (result.topMostZIndex === -Infinity) {
+            result.topMost = instance;
+
+            return result;
+          }
+        } else {
+          const zi = Number.parseInt(zIndex, 10) ?? 0;
+
+          if (result.topMostZIndex <= zi) {
+            result.topMostZIndex = zi;
+            result.topMost = instance;
+
+            return result;
+          }
+        }
+
+        return result;
+      },
+      { topMost: null, topMostZIndex: -Infinity },
+    );
+
+    return topMost;
+  }
+
+  private _tryFocus(element: HTMLElement | null) {
+    try {
+      runAfterRepaint(() => {
+        this._ignoreFocusChanges = true;
+
+        element?.focus();
+
+        this._ignoreFocusChanges = false;
+      });
+    } catch {
+      /* Squelch! */
+    }
   }
 
   private _gatherFocusableElements(root: Node) {
@@ -120,17 +189,40 @@ class FocusTrapper implements ReactiveController {
     const [firstFocusableChild, lastFocusableChild] =
       this._getFirstAndLastFocusableChildren();
 
-    firstFocusableChild?.focus();
+    this._tryFocus(firstFocusableChild);
 
     if (document.activeElement === this._lastFocused) {
-      lastFocusableChild?.focus();
+      this._tryFocus(lastFocusableChild);
     }
 
     if (document.activeElement && !root.contains(document.activeElement)) {
-      firstFocusableChild?.focus();
+      (document.activeElement as HTMLElement).blur();
     }
 
     this._lastFocused = document.activeElement as HTMLElement | null;
+  }
+
+  private _handleDocumentFocus(event: FocusEvent) {
+    if (!this._isEnabled) return;
+    if (this._ignoreFocusChanges) return;
+    if (this._topMostInstance !== this._host) return;
+
+    const root = this._resolveTreeRoot();
+
+    if (!root) return;
+
+    const target = event.target as HTMLElement;
+
+    if (this._host.contains(target)) {
+      if (target === this._host) {
+        const activeElement = getDeepActiveElement(1);
+
+        if (!activeElement) return;
+        if (!root.contains(activeElement)) return this._trap(root);
+
+        this._lastFocused = activeElement;
+      } else this._lastFocused = target;
+    } else this._trap(root);
   }
 
   public wrap(tree: TemplateResult) {
@@ -151,50 +243,34 @@ class FocusTrapper implements ReactiveController {
         aria-hidden="true"
         tabindex=${this._isEnabled ? "0" : "-1"}
         style=${styles}
+        data-trapper
         data-trapper-type=${TrapperTypes.START}
+        @focus=${this.sendFocus}
       ></div>
       ${tree}
       <div
         aria-hidden="true"
         tabindex=${this._isEnabled ? "0" : "-1"}
         style=${styles}
+        data-trapper
         data-trapper-type=${TrapperTypes.END}
+        @focus=${this.sendFocus}
       ></div>
     `;
   }
 
-  private _handleDocumentFocus(event: FocusEvent) {
-    if (this._ignoreFocusChanges) return;
-    if (!this._isEnabled) return;
-
-    const root = this._resolveTreeRoot();
-
-    if (!root) return;
-
-    const target = event.target as HTMLElement;
-
-    if (this._host.contains(target)) {
-      if (target === this._host) {
-        const activeElement = getDeepActiveElement(1);
-
-        if (!activeElement) return;
-        if (!root.contains(activeElement)) return this._trap(root);
-
-        this._lastFocused = activeElement;
-      } else this._lastFocused = target;
-    } else this._trap(root);
-  }
-
   public hostConnected() {
-    document.addEventListener("focus", this._handleDocumentFocus, {
-      capture: true,
-    });
+    this._host.setAttribute(HOST_INSTANCE_DATA_ATTRIBUTE, "");
+    this._host.setAttribute(HOST_ENABLED_DATA_ATTRIBUTE, `${this.enabled}`);
+
+    document.addEventListener("focusin", this._handleDocumentFocus);
   }
 
   public hostDisconnected() {
-    document.removeEventListener("focus", this._handleDocumentFocus, {
-      capture: true,
-    });
+    this._host.removeAttribute(HOST_INSTANCE_DATA_ATTRIBUTE);
+    this._host.removeAttribute(HOST_ENABLED_DATA_ATTRIBUTE);
+
+    document.removeEventListener("focusin", this._handleDocumentFocus);
   }
 
   public hostUpdate() {}
