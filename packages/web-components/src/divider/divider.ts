fd78fd3c7b6117acdb44b530e0323956882fb613
mimshins <mostafa.sh.coderino@gmail.com>
2024-12-28 18:38:48 +0330

    fix(divider): resolve the missing of variant reflection

--- packages/web-components/src/divider/divider.ts
+++ packages/web-components/src/divider/divider.ts
@@ -5,7 +5,7 @@ export class Divider extends LitElement {
   /**
    * The thickness of the divider.
    */
-  @property({ type: String })
+  @property({ type: String, reflect: true })
   public variant: "thin" | "medium" | "thick" = "thin";
 
   public override connectedCallback() {
