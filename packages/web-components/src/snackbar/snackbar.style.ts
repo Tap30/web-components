af2ab40c6978f27eb70fe46991bb26426a51d1e7
mimshins <mostafa.sh.coderino@gmail.com>
2025-01-01 21:19:55 +0330

    update spacings

--- packages/web-components/src/snackbar/snackbar.style.ts
+++ packages/web-components/src/snackbar/snackbar.style.ts
@@ -22,73 +22,6 @@ export default css`
     opacity: 1;
   }
 
-  .root.open .container {
-    transform: translateY(0) translateX(-50%);
-  }
-
-  .root {
-    position: absolute;
-    top: 0;
-    left: 0;
-    right: 0;
-
-    -moz-backface-visibility: hidden;
-    -webkit-backface-visibility: hidden;
-    backface-visibility: hidden;
-
-    z-index: ${Z_INDEXES[5]};
-  }
-
-  .container {
-    overflow: hidden;
-    z-index: 1;
-
-    transform: translateY(calc(-1 * (100% + var(--tapsi-spacing-6))))
-      translateX(-50%);
-
-    position: fixed;
-    left: 50%;
-    top: var(--tapsi-spacing-6);
-
-    background-color: var(--snackbar-background-color);
-    font-family: var(--tapsi-font-family);
-    border-radius: var(--tapsi-radius-full);
-
-    transition: transform 240ms ease;
-
-    display: flex;
-    align-items: center;
-    vertical-align: middle;
-
-    min-width: 20.5rem;
-    width: calc(100vw - var(--tapsi-spacing-9));
-    max-width: 27.5rem;
-
-    margin: auto;
-  }
-
-  .body {
-    padding: 0.875rem;
-
-    display: flex;
-    align-items: center;
-    vertical-align: middle;
-
-    width: 100%;
-  }
-
-  .text {
-    color: var(--snackbar-title-color);
-
-    font-size: var(--tapsi-typography-body-md-size);
-    font-weight: var(--tapsi-typography-body-md-weight);
-    line-height: var(--tapsi-typography-body-md-height);
-
-    flex: 1;
-
-    padding: 0 0.875rem;
-  }
-
   .root.inverse {
     --snackbar-icon-color: var(--tapsi-color-content-on-inverse);
     --snackbar-title-color: var(--tapsi-color-content-on-inverse);
@@ -120,12 +53,77 @@ export default css`
     --snackbar-background-color: var(--tapsi-color-surface-accent);
   }
 
+  .root.open .container {
+    transform: translateY(0) translateX(-50%);
+  }
+
+  .root {
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+
+    -moz-backface-visibility: hidden;
+    -webkit-backface-visibility: hidden;
+    backface-visibility: hidden;
+
+    z-index: ${Z_INDEXES[5]};
+  }
+
+  .container {
+    overflow: hidden;
+    z-index: 1;
+
+    position: fixed;
+    left: 50%;
+    top: var(--tapsi-spacing-6);
+
+    background-color: var(--snackbar-background-color);
+    font-family: var(--tapsi-font-family);
+    border-radius: var(--tapsi-radius-full);
+
+    display: flex;
+    align-items: center;
+
+    min-width: 20.5rem;
+    width: calc(100vw - var(--tapsi-spacing-9));
+    max-width: 27.5rem;
+
+    margin-right: auto;
+    margin-left: auto;
+
+    padding: var(--tapsi-spacing-5) var(--tapsi-spacing-6);
+
+    transform: translateY(calc(-1 * (100% + var(--tapsi-spacing-6))))
+      translateX(-50%);
+
+    transition: transform 240ms ease;
+  }
+
   .icon {
     color: var(--snackbar-icon-color);
     height: var(--tapsi-spacing-8);
     width: var(--tapsi-spacing-8);
 
     flex-shrink: 0;
+  }
+
+  .text {
+    color: var(--snackbar-title-color);
+
+    font-size: var(--tapsi-typography-body-md-size);
+    font-weight: var(--tapsi-typography-body-md-weight);
+    line-height: var(--tapsi-typography-body-md-height);
+
+    flex: 1;
+  }
+
+  .icon + .text {
+    margin-right: var(--tapsi-spacing-4);
+  }
+
+  .text + .dismiss {
+    margin-right: var(--tapsi-spacing-4);
   }
 
   .dismiss {
