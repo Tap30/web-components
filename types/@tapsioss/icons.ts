7c001f98bf1d55205c947b0071687433ef880a76
mimshins <mostafa.sh.coderino@gmail.com>
2025-01-27 12:37:48 +0330

    add type aliases

---
+++ types/@tapsioss/icons.ts
@@ -0,0 +1,17 @@
+// declare module "@tapsioss/icons" {
+type SVGPathInfo = {
+  clipRule?: string;
+  fillRule?: string;
+  xlinkHref?: string;
+  d: string;
+};
+
+type SVGIconInfo = {
+  kebabName: string;
+  pascalName: string;
+  paths: SVGPathInfo[];
+};
+
+declare const _default: Record<string, SVGIconInfo>;
+export default _default;
+// }
