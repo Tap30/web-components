0d7cbbb277b375d866f08747d9355c52358b5cd1
mimshins <mostafa.sh.coderino@gmail.com>
2025-02-19 13:21:09 +0330

    introduce the usage of test helpers

--- packages/web-components/src/switch/switch.test.ts
+++ packages/web-components/src/switch/switch.test.ts
@@ -1,6 +1,18 @@
-import { describe, expect, render, test } from "@internals/test-helpers";
+import {
+  afterEach,
+  describe,
+  disposeMocks,
+  expect,
+  render,
+  setupMocks,
+  test,
+} from "@internals/test-helpers";
 
 describe("switch", () => {
+  afterEach(async ({ page }) => {
+    await disposeMocks(page);
+  });
+
   test("T1", async ({ page }) => {
     await page.goto("/");
 
@@ -8,9 +20,23 @@ describe("switch", () => {
 
     const myEl = page.getByTestId("testtest");
 
+    const mocks = await setupMocks(page);
+    const fn = mocks.createFakeFn();
+
+    await mocks.events.attachMockedEvent(myEl, "click", fn.ref);
+
     await expect(page).toHaveURL("http://localhost:5173");
     await expect(page).toHaveTitle("Playground");
 
     await expect(myEl).toHaveText("hello");
+
+    await myEl.click();
+    await myEl.click();
+    await myEl.click();
+    await myEl.click();
+    await myEl.click();
+    await myEl.click();
+
+    await fn.matchResult({ called: true, callCount: 6 });
   });
 });
