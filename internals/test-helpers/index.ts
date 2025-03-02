37ecd0ad875ec7065eebe400b94396148935a2f5
mimshins <mostafa.sh.coderino@gmail.com>
2025-03-02 16:37:07 +0330

    extend base test module

--- internals/test-helpers/index.ts
+++ internals/test-helpers/index.ts
@@ -1,4 +1,11 @@
-import { test } from "@playwright/test";
+import { test as base } from "@playwright/test";
+
+const test = base.extend({
+  page: async ({ page }, use) => {
+    await page.goto("/test");
+    await use(page);
+  },
+});
 
 const { afterAll, afterEach, beforeAll, beforeEach, describe, expect, step } =
   test;
