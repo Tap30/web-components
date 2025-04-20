6fcaeba0fcb14303d3701a1a978467015f8266ee
Amirhossein Alibakhshi <amir78729@gmail.com>
2025-04-20 11:12:59 +0330

    update playground template

--- playground/src/index.ts
+++ playground/src/index.ts
@@ -1,4 +1,24 @@
 import "@tapsioss/theme/css-variables";
+/*
+Import modules using path aliases.
 
-// Import modules using path aliases
-// i.e. `import "@tapsioss/web-components/avatar";`
+Option 1: Register elements manually.
+i.e.:
+```
+import { registerAvatarElement } from "@tapsioss/web-components";
+register();
+```
+
+Option 2: Register elements automatically.
+i.e.:
+```
+import "@tapsioss/web-components/avatar/element";
+```
+
+Option 3: Register all the components.
+i.e.:
+```
+import { registerAllElements } from "@tapsioss/web-components";
+registerAllElements();
+```
+*/
